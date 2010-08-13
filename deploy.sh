f299812d97effe40affbfebf863640a2f5a162cd
Koen Deforche <koen@emweb.be>
2010-08-13 17:11:57 +0200

    update installation documentation

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "Installing example ${APPBIN} to ${DEPLOYROOT}/${APPNAME}/ ..."
-mkdir -p ${DEPLOYROOT}/${APPNAME}
-install ${APPBIN} ${DEPLOYROOT}/${APPNAME}
-if [ "${APPRESOURCES}" ]; then
-  cp -R ${APPRESOURCES} ${DEPLOYROOT}/${APPNAME}
-fi
-cp -R ${WT_SOURCE_DIR}/resources ${DEPLOYROOT}/${APPNAME}
-echo "Done."
-
