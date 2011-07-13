0dc0022598601d1a93f955bf6959d5f1d59a82f6
Koen Deforche <koen@emweb.be>
2011-07-13 11:51:02 +0200

    ... and a test case

---
+++ test/models/WStandardItemModelTest.C
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2010 Emweb bvba, Kessel-Lo, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+#include <boost/test/unit_test.hpp>
+
+#include <Wt/WStandardItemModel>
+#include <Wt/WStandardItem>
+
+using namespace Wt;
+
+BOOST_AUTO_TEST_CASE( standarditems_test1 )
+{
+  WStandardItemModel *model = new WStandardItemModel();
+
+  BOOST_REQUIRE(model->columnCount() == 0);
+  BOOST_REQUIRE(model->rowCount() == 0);
+
+  std::vector<WStandardItem *> items;
+  items.push_back(new WStandardItem());
+  items.push_back(0);
+
+  model->insertColumn(0, items);
+
+  BOOST_REQUIRE(model->columnCount() == 1);
+  BOOST_REQUIRE(model->rowCount() == 2);
+
+  model->clear();
+}
