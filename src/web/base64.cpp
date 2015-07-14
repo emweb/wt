9dd29eabbfe8aff214e4e396bb1e03e4e8632faf
Pau Garcia i Quiles <pgquiles@elpauer.org>
2015-07-14 19:33:43 +0200

    Fix FTBFS on arm64 due to 'char' meaning 'unsigned char' on arm64, while 'signed char' on x86. Fixes Debian bug #791417

--- src/web/base64.cpp
+++ src/web/base64.cpp
@@ -2,7 +2,7 @@
 
 namespace base64
 {
-  const char _to_table[64] =
+  const signed char _to_table[64] =
   { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
     'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
@@ -12,13 +12,13 @@ namespace base64
     'w', 'x', 'y', 'z', '0', '1', '2', '3',
     '4', '5', '6', '7', '8', '9', '+', '/'
   };
-  const char* to_table = _to_table;
+  const signed char* to_table = _to_table;
 
 
-  const char* to_table_end = 
+  const signed char* to_table_end = 
     _to_table + sizeof(_to_table);
 
-  const char _from_table[128] =
+  const signed char _from_table[128] =
   {
     -1, -1, -1, -1, -1, -1, -1, -1, // 0
     -1, -1, -1, -1, -1, -1, -1, -1, // 8
@@ -37,5 +37,5 @@ namespace base64
     41, 42, 43, 44, 45, 46, 47, 48, // 112
     49, 50, 51, -1, -1, -1, -1, -1  // 120
   };
-  const char* from_table = _from_table;
+  const signed char* from_table = _from_table;
 }
