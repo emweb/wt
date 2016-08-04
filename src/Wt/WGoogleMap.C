8eb233c7c9d7a5487ec157c3db65eba0644b1063
Cody <cody@nfiinix.com>
2016-08-04 08:31:53 -0700

    we must include API key as URL parameter in maps api request

--- src/Wt/WGoogleMap.C
+++ src/Wt/WGoogleMap.C
@@ -247,7 +247,7 @@ void WGoogleMap::render(WFlags<RenderFlag> flags)
 
     strm << "setTimeout(function(){ delete " << initFunction << ";}, 0)};"
 	 << "google.load(\"maps\", \"" << (apiVersion_ == Version2 ? '2' : '3')
-	 << "\", {other_params:\"sensor=false\", callback: "
+	 << "\", {other_params:\"key=" << googlekey << "\", callback: "
 	 << initFunction << "});"
 	 << "}"; // private scope
 
