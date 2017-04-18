a712d0c32eb2680088037641cb0c2cf180d374fd
Roel Standaert <roel@emweb.be>
2017-04-18 12:51:44 +0200

    Several changes:
     - WPopupWidget/WPopupMenu: Call bind/unbind function with the right object
     - Json move semantics and initializer list syntax

--- src/Wt/Json/Value.C
+++ src/Wt/Json/Value.C
@@ -113,7 +113,7 @@ Value::Value(const Object& value)
 { }
 
 Value::Value(Type type)
-{ 
+{
   switch (type) {
   case NullType: break;
   case BoolType: v_ = false; break;
@@ -127,6 +127,38 @@ Value::Value(Type type)
 Value::Value(const Value& other)
   : v_(other.v_)
 { }
+
+#ifdef WT_CXX11
+Value::Value(Value&& other)
+  : v_(std::move(other.v_))
+{ }
+
+Value::Value(Object&& other)
+  : v_(std::move(other))
+{ }
+
+Value::Value(Array&& other)
+  : v_(std::move(other))
+{ }
+
+Value& Value::operator= (Value&& other)
+{
+  v_ = std::move(other.v_);
+  return *this;
+}
+
+Value& Value::operator= (Object&& other)
+{
+  v_ = std::move(other);
+  return *this;
+}
+
+Value& Value::operator= (Array&& other)
+{
+  v_ = std::move(other);
+  return *this;
+}
+#endif
 
 Value& Value::operator= (const Value& other)
 {
