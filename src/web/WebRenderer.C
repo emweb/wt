5fb75545af12f091642b55a0ba7c2e6ccd0a8ab1
Roel Standaert <roel@emweb.be>
2018-06-20 16:40:57 +0200

    Issue #6438: rethrow exception properly (without copy)

--- src/web/WebRenderer.C
+++ src/web/WebRenderer.C
@@ -1722,9 +1722,9 @@ void WebRenderer::collectJavaScriptUpdate(WStringStream& out)
     app->renderedInternalPath_ = app->newInternalPath_;
 
     updateLoadIndicator(out, app, false);
-  } catch (std::exception &e) {
+  } catch (const std::exception &e) {
     out << '}';
-    throw e;
+    throw;
   } catch (...) {
     out << '}';
     throw;
