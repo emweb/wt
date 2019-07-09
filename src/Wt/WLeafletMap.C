6d65759d4b5d65efe93ff7477e07eed4610f2200
Roel Standaert <roel@emweb.be>
2019-07-09 11:58:49 +0200

    WLeafletMap changes:
     - Use transform to center marker, and to change anchor point
    
       Setting iconSize and iconAnchor to null will cause none of leaflet's own
       sizing and positioning code to be applied, so we can use transform on
       our own container to set the anchor point, and change it at runtime.
    
       A nice thing about using CSS transforms is that we don't need to
       actually know the width and height of the widget, we can just use -50%
       and let the browser determine how many pixels that is.
    
     - Make leaflet widget markers non-interactive, yet use CSS to make the wigdget itself interactive
    
       This will allow events to be fired on the widget itself, but makes sure
       that the divs containing it don't catch the event. Could be useful if it
       is desirable to limit interactivity to only some parts of the widget
       (and otherwise let other markers visually behind it catch it)

--- src/Wt/WLeafletMap.C
+++ src/Wt/WLeafletMap.C
@@ -10,6 +10,7 @@
 #include "Wt/WBrush"
 #include "Wt/WColor"
 #include "Wt/WContainerWidget"
+#include "Wt/WCssStyleSheet"
 #include "Wt/WJavaScriptPreamble"
 #include "Wt/WLink"
 #include "Wt/WLogger"
@@ -29,9 +30,22 @@
 #include "js/WLeafletMap.min.js"
 #endif
 
+#ifdef WT_CXX11
+#include <memory>
+#define SCOPED_PTR std::unique_ptr
+#else
+#include <boost/scoped_ptr.hpp>
+#define SCOPED_PTR boost::scoped_ptr
+#endif
+
 namespace Wt {
 
 LOGGER("WLeafletMap");
+
+const std::string WLeafletMap::WIDGETMARKER_CONTAINER_RULENAME = "WLeafletMap::WidgetMarker::container";
+const std::string WLeafletMap::WIDGETMARKER_CONTAINER_CHILDREN_RULENAME = "WLeafletMap::WidgetMarker::container-children";
+
+#define WIDGETMARKER_CONTAINER_CLASS "Wt-leaflet-widgetmarker-container"
 
 class WLeafletMap::Impl : public WWebWidget {
 public:
@@ -240,12 +254,21 @@ void WLeafletMap::Marker::setMap(WLeafletMap *map)
 void WLeafletMap::Marker::unrender()
 { }
 
+bool WLeafletMap::Marker::needsUpdate() const
+{
+  return false;
+}
+
+void WLeafletMap::Marker::update(WStringStream &js)
+{ }
+
 WLeafletMap::WidgetMarker::WidgetMarker(const Coordinate &pos,
                                         WWidget *widget)
   : Marker(pos),
     container_(0),
     anchorX_(-1),
-    anchorY_(-1)
+    anchorY_(-1),
+    anchorPointChanged_(false)
 {
   createContainer();
   container_->addWidget(widget);
@@ -300,34 +323,24 @@ void WLeafletMap::WidgetMarker::setMap(WLeafletMap *map)
 
 void WLeafletMap::WidgetMarker::createMarkerJS(WStringStream &ss, WStringStream &postJS) const
 {
-  DomElement *element = container_->createSDomElement(WApplication::instance());
+  SCOPED_PTR<DomElement> element(container_->createSDomElement(WApplication::instance()));
 
   DomElement::TimeoutList timeouts;
 
+  char buf[30];
+
+  if (anchorX_ >= 0 || anchorY_ >= 0) {
+    updateAnchorJS(postJS);
+  }
+
   EscapeOStream js(postJS);
-  // FIXME: allow control over disabling/enabling pointerdown propagation?
-  js << "var o=" << container_->jsRef() << ";if(o){"
-        "" "o.addEventListener('pointerdown',function(e){e.stopPropagation();},false);"
-        "}";
 
   EscapeOStream es(ss);
-  char buf[30];
   es << "(function(){";
   es << "var wIcon=L.divIcon({"
-        "className:'',";
-  if (!widget()->width().isAuto() &&
-      !widget()->height().isAuto()) {
-    es << "iconSize:[";
-    es << Utils::round_js_str(widget()->width().toPixels(), 16, buf) << ',';
-    es << Utils::round_js_str(widget()->height().toPixels(), 16, buf) << "],";
-    if (anchorX_ >= 0 || anchorY_ >= 0) {
-      double x = anchorX_ >= 0 ? anchorX_ : widget()->width().toPixels() / 2.0;
-      double y = anchorY_ >= 0 ? anchorY_ : widget()->height().toPixels() / 2.0;
-      es << "iconAnchor:[";
-      es << Utils::round_js_str(x, 16, buf) << ',';
-      es << Utils::round_js_str(y, 16, buf) << "],";
-    }
-  }
+        "className:'',"
+        "iconSize:null,"
+        "iconAnchor:null,";
   es << "html:'";
   es.pushEscape(EscapeOStream::JsStringLiteralSQuote);
   element->asHTML(es, js, timeouts);
@@ -337,17 +350,21 @@ void WLeafletMap::WidgetMarker::createMarkerJS(WStringStream &ss, WStringStream 
   es << Utils::round_js_str(position().latitude(), 16, buf) << ",";
   es << Utils::round_js_str(position().longitude(), 16, buf) << "],";
   es << "{"
+          "interactive:false,"
           "icon:wIcon,"
           "keyboard:false"
         "});})()";
-
-  delete element;
 }
 
 void WLeafletMap::WidgetMarker::setAnchorPoint(double x, double y)
 {
   anchorX_ = x;
   anchorY_ = y;
+
+  if (map() && map()->isRendered()) {
+    anchorPointChanged_ = true;
+    map()->scheduleRender();
+  }
 }
 
 void WLeafletMap::WidgetMarker::unrender()
@@ -370,7 +387,41 @@ void WLeafletMap::WidgetMarker::unrender()
 void WLeafletMap::WidgetMarker::createContainer()
 {
   container_ = new Wt::WContainerWidget();
+  container_->addStyleClass(WIDGETMARKER_CONTAINER_CLASS);
   container_->setJavaScriptMember("wtReparentBarrier", "true");
+}
+
+bool WLeafletMap::WidgetMarker::needsUpdate() const
+{
+  return anchorPointChanged_;
+}
+
+void WLeafletMap::WidgetMarker::update(WStringStream &js)
+{
+  if (anchorPointChanged_) {
+    updateAnchorJS(js);
+    anchorPointChanged_ = false;
+  }
+}
+
+void WLeafletMap::WidgetMarker::updateAnchorJS(WStringStream &js) const
+{
+  char buf[30];
+  js << "var o=" << container_->jsRef() << ";if(o){"
+        "" "o.style.transform='translate(";
+  if (anchorX_ >= 0) {
+    js << Utils::round_js_str(-anchorX_, 16, buf) << "px";
+  } else {
+    js << "-50%";
+  }
+  js << ',';
+  if (anchorY_ >= 0) {
+    js << Utils::round_js_str(-anchorY_, 16, buf) << "px";
+  } else {
+    js << "-50%";
+  }
+  js << ")';"
+        "}";
 }
 
 WLeafletMap::LeafletMarker::LeafletMarker(const Coordinate &pos)
@@ -426,6 +477,13 @@ void WLeafletMap::setup()
 
   WApplication *app = WApplication::instance();
   if (app) {
+    if (!app->styleSheet().isDefined(WIDGETMARKER_CONTAINER_RULENAME)) {
+      app->styleSheet().addRule("." WIDGETMARKER_CONTAINER_CLASS, "transform: translate(-50%, -50%);", WIDGETMARKER_CONTAINER_RULENAME);
+    }
+    if (!app->styleSheet().isDefined(WIDGETMARKER_CONTAINER_CHILDREN_RULENAME)) {
+      app->styleSheet().addRule("." WIDGETMARKER_CONTAINER_CLASS " > *", "pointer-events: auto;", WIDGETMARKER_CONTAINER_CHILDREN_RULENAME);
+    }
+
     std::string leafletJSURL;
     std::string leafletCSSURL;
     Wt::WApplication::readConfigurationProperty("leafletJSURL", leafletJSURL);
@@ -774,8 +832,13 @@ void WLeafletMap::render(WFlags<RenderFlag> flags)
         markers_[i].flags.test(MarkerEntry::BIT_ADDED)) {
       addMarkerJS(ss, markers_[i].id, markers_[i].marker);
       markers_[i].flags.reset(MarkerEntry::BIT_ADDED);
-    } else if (markers_[i].marker->moved_) {
-      moveMarkerJS(ss, markers_[i].id, markers_[i].marker->position());
+    } else {
+      if (markers_[i].marker->moved_) {
+        moveMarkerJS(ss, markers_[i].id, markers_[i].marker->position());
+      }
+      if (markers_[i].marker->needsUpdate()) {
+        markers_[i].marker->update(ss);
+      }
     }
     markers_[i].marker->moved_ = false;
   }
