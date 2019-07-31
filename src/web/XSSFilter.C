f4982ee9e2f810a3a340dd5769426d941f7b5e20
Roel Standaert <roel@emweb.be>
2019-07-31 13:45:36 +0200

    That const_cast shortcut is not worth risking UB
    Doubly so because it actually *does* modify the string (the const_cast
    is lying)
    
    Also actually using the info that we get from rapidxml about the size of
    strings.

--- src/web/XSSFilter.C
+++ src/web/XSSFilter.C
@@ -24,11 +24,11 @@ void XSSSanitize(xml_node<> *x_node)
   for (xml_attribute<> *x_attr = x_node->first_attribute(); x_attr;) {
 
     xml_attribute<> *x_next_attr = x_attr->next_attribute();
+    std::string const name(x_attr->name(), x_attr->name_size());
+    std::string const value(x_attr->value(), x_attr->value_size());
     if (Wt::XSS::isBadAttribute(x_attr->name())
 	|| Wt::XSS::isBadAttributeValue(x_attr->name(), x_attr->value())) {
-      LOG_SECURE("discarding invalid attribute: "
-		 << (const char *)x_attr->name() << ": "
-		 << (const char *)x_attr->value());
+      LOG_SECURE("discarding invalid attribute: " << name << ": " << value);
       x_node->remove_attribute(x_attr);
     }
 
@@ -38,8 +38,9 @@ void XSSSanitize(xml_node<> *x_node)
   for (xml_node<> *x_child = x_node->first_node(); x_child;) {
     xml_node<> *x_next_child = x_child->next_sibling();
 
-    if (Wt::XSS::isBadTag(x_child->name())) {
-      LOG_SECURE("discarding invalid tag: " << (const char *)x_child->name());
+    std::string const name(x_child->name(), x_child->name_size());
+    if (Wt::XSS::isBadTag(name)) {
+      LOG_SECURE("discarding invalid tag: " << name);
       x_node->remove_node(x_child);
     } else
       XSSSanitize(x_child);
@@ -47,9 +48,10 @@ void XSSSanitize(xml_node<> *x_node)
     x_child = x_next_child;
   }
 
+  std::string const name(x_node->name(), x_node->name_size());
   if (!x_node->first_node()
       && x_node->value_size() == 0
-      && !DomElement::isSelfClosingTag(x_node->name())) {
+      && !DomElement::isSelfClosingTag(name)) {
     // We need to add an emtpy data node since <div /> is illegal HTML
     // (but valid XML / XHTML)
     xml_node<> *empty
@@ -64,14 +66,17 @@ bool XSSFilterRemoveScript(WString& text)
     return true;
 
   std::string result = "<span>" + text.toUTF8() + "</span>";
-  char *ctext = const_cast<char *>(result.c_str()); // Shhht it's okay !
+  std::vector<char> buf;
+  buf.reserve(result.size() + 1);
+  buf.insert(buf.end(), result.begin(), result.end());
+  buf.push_back('\0');
 
   try {
     xml_document<> doc;
     doc.parse<parse_comment_nodes
       | parse_validate_closing_tags
       | parse_validate_utf8
-      | parse_xhtml_entity_translation>(ctext);
+      | parse_xhtml_entity_translation>(&buf.front());
 
     XSSSanitize(doc.first_node());
 
