624fb6b6692b1613b3e59812586c04f9491e307c
Roel Standaert <roel@emweb.be>
2019-08-14 10:38:23 +0200

    Fixed documentation of WSpinBox/WDoubleSpinBox valueChanged()

--- src/Wt/WDoubleSpinBox.h
+++ src/Wt/WDoubleSpinBox.h
@@ -113,9 +113,13 @@ public:
 
   /*! \brief A %signal that indicates when the value has changed.
    *
-   * This signal is emitted when setValue() is called.
+   * This signal is emitted when changed() is emitted, but supplies the
+   * new value as an argument. The changed() signal is emitted
+   * when the user changes the value of the spinbox
+   * by pressing the up/down arrow, or entering a different value and pressing
+   * enter or moving focus.
    *
-   * \sa setValue()
+   * \sa changed()
    */
   Signal<double>& valueChanged() { return valueChanged_; }
 
