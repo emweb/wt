4c254d56389e074a4f4c0dabd819da6f1a780062
Roel Standaert <roel@emweb.be>
2020-01-20 12:47:08 +0100

    Log an error, but keep drawing, when drawImage fails

--- src/js/WPaintedWidget.js
+++ src/js/WPaintedWidget.js
@@ -657,6 +657,23 @@ WT_DECLARE_WT_MEMBER
 	    }
 	    return [x,y,w,h];
 	 };
+
+         this.drawImage = function(ctx,
+                                   image,
+                                   uri,
+                                   src_rect,
+                                   tgt_rect) {
+           try {
+             ctx.drawImage(image, src_rect[0], src_rect[1], src_rect[2], src_rect[3],
+                                  tgt_rect[0], tgt_rect[1], tgt_rect[2], tgt_rect[3]);
+           } catch (e) {
+             var msg = "Error while drawing image: '" + uri + "': " + e.name;
+             if (e.message) {
+               msg += ": " + e.message;
+             }
+             console.error(msg);
+           }
+         };
       }
 
       return new Utils();
