be31d400807da0b25b78a2f72971317af1b74c46
Roel Standaert <roel@emweb.be>
2020-02-19 17:02:46 +0100

    Mail client: support for TLS and auth (PLAIN/LOGIN)
    Final API TBD, but all parts functional, includes:
    
     - TLS:
      - Support for going straight to TLS (no STARTTLS),
        verified using stunnel + postfix
      - Support for STARTTLS, verified using postfix
     - AUTH:
      - PLAIN and LOGIN authentication methods,
        verified using postfix + saslauthd
    
    Relates to issue #7444

--- src/Wt/Mail/Client.C
+++ src/Wt/Mail/Client.C
@@ -8,10 +8,17 @@
 #include <Wt/AsioWrapper/asio.hpp>
 #include <Wt/AsioWrapper/system_error.hpp>
 
+#ifdef WT_WITH_SSL
+#include <Wt/AsioWrapper/ssl.hpp>
+
+#include "web/SslUtils.h"
+#endif // WT_WITH_SSL
+
 #include "Client.h"
 #include "Message.h"
 #include "Wt/WApplication.h"
 #include "Wt/WException.h"
+#include "Wt/Utils.h"
 
 #include "WebUtils.h"
 
@@ -28,19 +35,71 @@ namespace asio = AsioWrapper::asio;
   namespace Mail {
 
 using asio::ip::tcp;
+#ifdef WT_WITH_SSL
+namespace ssl = asio::ssl;
+#endif // WT_WITH_SSL
 
-class Client::Impl
+class Client::BaseImpl
 {
 public:
-  enum class ReplyCode {
+  enum class ReplyCode : int {
     Ready = 220,
     Bye = 221,
+    AuthSuccessful = 235,
     Ok = 250,
+    AuthContinue = 334,
     StartMailInput = 354
   };
 
-  Impl(const std::string& host, const std::string& selfFQDN, int port)
-    : socket_(io_service_)
+  BaseImpl()
+  { }
+
+  virtual ~BaseImpl()
+  { }
+
+  virtual bool good() const = 0;
+  virtual bool send(const Message &message) = 0;
+};
+
+namespace {
+
+template<bool ssl>
+struct SocketData;
+
+template<>
+struct SocketData<false> {
+  SocketData(asio::io_service &io_service, bool verifyCerts)
+    : socket_(io_service)
+  {
+    (void)verifyCerts;
+  }
+
+  tcp::socket socket_;
+};
+#ifdef WT_WITH_SSL
+template<>
+struct SocketData<true> {
+  SocketData(asio::io_service &io_service, bool verifyCerts)
+    : ssl_ctx_(Ssl::createSslContext(io_service, verifyCerts)),
+      stream_(io_service, ssl_ctx_),
+      encrypted_(false)
+  { }
+
+  ssl::context ssl_ctx_;
+  ssl::stream<tcp::socket> stream_;
+  bool encrypted_;
+};
+#endif // WT_WITH_SSL
+
+}
+
+template<bool ssl>
+class Client::Impl : public Client::BaseImpl {
+public:
+  Impl(const Client::Configuration &config,
+       const std::string& host,
+       int port)
+    : data_(io_service_, config.certificateVerificationEnabled_)
   {
     // Get a list of endpoints corresponding to the server name.
     tcp::resolver resolver(io_service_);
@@ -52,48 +111,56 @@ public:
     // Try each endpoint until we successfully establish a connection.
     AsioWrapper::error_code error = asio::error::host_not_found;
     while (error && endpoint_iterator != end) {
-      socket_.close();
-      socket_.connect(*endpoint_iterator++, error);
+      close();
+      socket().connect(*endpoint_iterator++, error);
     }
 
     if (error) {
-      socket_.close();
+      close();
       LOG_ERROR("could not connect to: " << host << ":" << port);
       return;
     }
 
     try {
+      doInitTLS(config, host);
+
       failIfReplyCodeNot(ReplyCode::Ready);
 
-      send("EHLO " + selfFQDN + "\r\n");
+      send("EHLO " + config.selfHost_ + "\r\n");
 
       failIfReplyCodeNot(ReplyCode::Ok);
+
+      doStartTLS(config, host);
+
+      doAuth(config);
     } catch (std::exception& e) {
-      socket_.close();
+      close();
       LOG_ERROR(e.what());
       return;
     }
   }
 
-  bool good() const {
-    return socket_.is_open();
-  }
-
-  ~Impl()
+  virtual ~Impl() override
   {
     if (good()) {
       try {
-	send("QUIT\r\n");
-	failIfReplyCodeNot(ReplyCode::Bye);
+        send("QUIT\r\n");
+        failIfReplyCodeNot(ReplyCode::Bye);
+        close();
       } catch (std::exception& e) {
-	socket_.close();
-	LOG_ERROR(e.what());
-	return;
+        close();
+        LOG_ERROR(e.what());
+        return;
       }
     }
   }
 
-  bool send(const Message& message)
+  virtual bool good() const override
+  {
+    return socket().is_open();
+  }
+
+  virtual bool send(const Message& message) override
   {
     try {
       send("MAIL FROM:<" + message.from().address() + ">\r\n");
@@ -113,11 +180,11 @@ public:
       message.write(data);
       data << ".\r\n";
 
-      asio::write(socket_, buf);
+      write(buf);
 
       failIfReplyCodeNot(ReplyCode::Ok);
     } catch (std::exception& e) {
-      socket_.close();
+      close();
       LOG_ERROR(e.what());
       return false;
     }
@@ -125,11 +192,61 @@ public:
   }
 
 private:
+  void doAuth(const Client::Configuration &config)
+  {
+    if (config.username_.empty() ||
+        config.password_.empty())
+      return;
+
+    if (config.authenticationMethod_ == AuthenticationMethod::Plain)
+      doPlainAuth(config);
+    else if (config.authenticationMethod_ == AuthenticationMethod::Login)
+      doLoginAuth(config);
+    else
+      assert(false);
+  }
+
+  void doPlainAuth(const Client::Configuration &config)
+  {
+    assert(config.authenticationMethod_ == AuthenticationMethod::Plain);
+
+    const std::string authStr = '\0' + config.username_ + '\0' + config.password_;
+    const std::string authStrBase64 = Utils::base64Encode(authStr, false) + "\r\n";
+
+    send("AUTH PLAIN\r\n");
+
+    failIfReplyCodeNot(ReplyCode::AuthContinue);
+
+    send(authStrBase64);
+
+    failIfReplyCodeNot(ReplyCode::AuthSuccessful);
+  }
+
+  void doLoginAuth(const Client::Configuration &config)
+  {
+    assert(config.authenticationMethod_ == AuthenticationMethod::Login);
+
+    const std::string usernameBase64 = Utils::base64Encode(config.username_, false) + "\r\n";
+    const std::string passwordBase64 = Utils::base64Encode(config.password_, false) + "\r\n";
+
+    send("AUTH LOGIN\r\n");
+
+    failIfReplyCodeNot(ReplyCode::AuthContinue);
+
+    send(usernameBase64);
+
+    failIfReplyCodeNot(ReplyCode::AuthContinue);
+
+    send(passwordBase64);
+
+    failIfReplyCodeNot(ReplyCode::AuthSuccessful);
+  }
+
   void send(const std::string& s)
   {
     LOG_DEBUG("C " << s);
     // asio::const_buffer request = asio::buffer(s);
-    asio::write(socket_, asio::buffer(s));
+    write(asio::buffer(s));
 
     // FIXME error handling ?
   }
@@ -147,7 +264,7 @@ private:
 
     asio::streambuf response;
     for (;;) {
-      asio::read_until(socket_, response, "\r\n");
+      read_until(response, "\r\n");
 
       std::istream in(&response);
       int code;
@@ -175,30 +292,220 @@ private:
     return -1; // Not reachable
   }
 
+  template<typename Buffer>
+  void read_until(Buffer &&buf, const char *str);
+  template<typename Buffer>
+  void write(Buffer &&buf);
+  void close();
+  void handshake(const Client::Configuration &config,
+                 const std::string &hostName);
+  void doInitTLS(const Client::Configuration &config,
+                 const std::string &hostName);
+  void doStartTLS(const Client::Configuration &config,
+                  const std::string &hostName);
+
+  tcp::socket & socket();
+  const tcp::socket & socket() const;
+
 private:
   asio::io_service io_service_;
-  tcp::socket socket_;
+  SocketData<ssl> data_;
 };
 
+template<>
+template<typename Buffer>
+void Client::Impl<false>::read_until(Buffer &&buf, const char *str)
+{
+  asio::read_until(data_.socket_, buf, str);
+}
+
+template<>
+template<typename Buffer>
+void Client::Impl<false>::write(Buffer &&buf)
+{
+  asio::write(data_.socket_, buf);
+}
+
+template<>
+void Client::Impl<false>::close()
+{
+  if (!data_.socket_.is_open())
+    return;
+
+  AsioWrapper::error_code ignored;
+  data_.socket_.shutdown(tcp::socket::shutdown_both, ignored);
+  data_.socket_.close(ignored);
+}
+
+template<>
+void Client::Impl<false>::doInitTLS(const Client::Configuration &config,
+                                    const std::string &hostName)
+{
+  // no TLS, do nothing
+  (void)config;
+  (void)hostName;
+}
+
+template<>
+void Client::Impl<false>::doStartTLS(const Client::Configuration &config,
+                                     const std::string &hostName)
+{
+  // no TLS, do nothing
+  (void)config;
+  (void)hostName;
+}
+
+template<>
+tcp::socket & Client::Impl<false>::socket()
+{
+  return data_.socket_;
+}
+
+template<>
+const tcp::socket & Client::Impl<false>::socket() const
+{
+  return data_.socket_;
+}
+
+#ifdef WT_WITH_SSL
+template<>
+template<typename Buffer>
+void Client::Impl<true>::read_until(Buffer &&buf, const char *str)
+{
+  if (data_.encrypted_) {
+    asio::read_until(data_.stream_, buf, str);
+  } else {
+    asio::read_until(data_.stream_.next_layer(), buf, str);
+  }
+}
+
+template<>
+template<typename Buffer>
+void Client::Impl<true>::write(Buffer &&buf)
+{
+  if (data_.encrypted_) {
+    asio::write(data_.stream_, buf);
+  } else {
+    asio::write(data_.stream_.next_layer(), buf);
+  }
+}
+
+template<>
+void Client::Impl<true>::close()
+{
+  if (!data_.stream_.next_layer().is_open())
+    return;
+
+  AsioWrapper::error_code ignored;
+  if (data_.encrypted_) {
+    data_.stream_.shutdown(ignored);
+  }
+  data_.stream_.next_layer().shutdown(tcp::socket::shutdown_both, ignored);
+  data_.stream_.next_layer().close(ignored);
+}
+
+template<>
+void Client::Impl<true>::handshake(const Client::Configuration &config,
+                                   const std::string &hostName)
+{
+  if (config.certificateVerificationEnabled_) {
+    data_.stream_.set_verify_mode(ssl::verify_peer);
+    LOG_DEBUG("verifying that peer is " << hostName);
+    data_.stream_.set_verify_callback
+        (ssl::rfc2818_verification(hostName));
+  }
+  data_.stream_.handshake(ssl::stream_base::client);
+}
+
+template<>
+void Client::Impl<true>::doInitTLS(const Client::Configuration &config,
+                                   const std::string &hostName)
+{
+  if (config.transportEncryption_ != TransportEncryption::TLS)
+    return;
+
+  data_.encrypted_ = true;
+
+  handshake(config, hostName);
+}
+
+template<>
+void Client::Impl<true>::doStartTLS(const Client::Configuration &config,
+                                    const std::string &hostName)
+{
+  if (config.transportEncryption_ != TransportEncryption::StartTLS)
+    return;
+
+  send("STARTTLS\r\n");
+
+  failIfReplyCodeNot(ReplyCode::Ready);
+
+  data_.encrypted_ = true;
+
+  handshake(config, hostName);
+
+  send("EHLO " + config.selfHost_ + "\r\n");
+
+  failIfReplyCodeNot(ReplyCode::Ok);
+}
+
+template<>
+tcp::socket & Client::Impl<true>::socket()
+{
+  return data_.stream_.next_layer();
+}
+
+template<>
+const tcp::socket & Client::Impl<true>::socket() const
+{
+  return data_.stream_.next_layer();
+}
+#endif // WT_WITH_SSL
+
 Client::Client(const std::string& selfHost)
-  : impl_(nullptr),
-    selfHost_(selfHost)
-{ 
-  if (selfHost_.empty()) {
-    selfHost_ = "localhost";
-    WApplication::readConfigurationProperty("smtp-self-host", selfHost_);
+  : configuration_({
+      selfHost,
+      "",
+      "",
+      AuthenticationMethod::Plain,
+      TransportEncryption::None,
+      true
+    })
+{
+  if (configuration_.selfHost_.empty()) {
+    configuration_.selfHost_ = "localhost";
+    WApplication::readConfigurationProperty("smtp-self-host", configuration_.selfHost_);
 
     if (!logged)
-      LOG_INFO("Smtp::Client: using '" << selfHost_ << "' (from smtp-self-host property) "
+      LOG_INFO("using '" << configuration_.selfHost_ << "' (from smtp-self-host property) "
 	       "as self host");
   } else
     if (!logged)
-      LOG_INFO("Smtp::Client: using '" << selfHost_ << "' as self host");
+      LOG_INFO("using '" << configuration_.selfHost_ << "' as self host");
 }
 
 Client::~Client()
 {
   disconnect();
+}
+
+void Client::enableAuthentication(const std::string &username,
+                                  const std::string &password,
+                                  AuthenticationMethod method)
+{
+  configuration_.username_ = username;
+  configuration_.password_ = password;
+  configuration_.authenticationMethod_ = method;
+}
+
+void Client::setSslCertificateVerificationEnabled(bool enabled)
+{
+  configuration_.certificateVerificationEnabled_ = enabled;
+}
+
+void Client::setTransportEncryption(TransportEncryption method)
+{
+  configuration_.transportEncryption_ = method;
 }
 
 bool Client::connect()
@@ -208,11 +515,12 @@ bool Client::connect()
   
   WApplication::readConfigurationProperty("smtp-host", smtpHost);
   WApplication::readConfigurationProperty("smtp-port", smtpPortStr);
+  // TODO(Roel): read auth config?
 
   int smtpPort = Utils::stoi(smtpPortStr);
 
   if (!logged)
-    LOG_INFO("Smtp::Client: using '" << smtpHost << ":" << smtpPortStr
+    LOG_INFO("using '" << smtpHost << ":" << smtpPortStr
 	     << "' (from smtp-host and smtp-port properties) as SMTP host");
 
   return connect(smtpHost, smtpPort);
@@ -221,19 +529,28 @@ bool Client::connect()
 bool Client::connect(const std::string& smtpHost, int smtpPort)
 {
   if (!logged) {
-    LOG_INFO("Smtp::Client: connecting to '" << smtpHost << ":" << smtpPort);
+    LOG_INFO("connecting to '" << smtpHost << ':' << smtpPort << '\'');
     logged = true;
   }
 
-  delete impl_;
-  impl_ = new Impl(smtpHost, selfHost_, smtpPort);
+  // Exception safety: make sure old impl is removed before we create a new one
+  impl_ = nullptr;
+  if (transportEncryption() == TransportEncryption::None) {
+    impl_ = cpp14::make_unique<Impl<false>>(configuration_, smtpHost, smtpPort);
+  } else {
+#ifdef WT_WITH_SSL
+    impl_ = cpp14::make_unique<Impl<true>>(configuration_, smtpHost, smtpPort);
+#else // WT_WITH_SSL
+    LOG_ERROR("TLS requested, but Wt built without OpenSSL");
+    return false;
+#endif // WT_WITH_SSL
+  }
 
   return impl_->good();
 }
 
 void Client::disconnect()
 {
-  delete impl_;
   impl_ = nullptr;
 }
 
--- src/Wt/Mail/Client.h
+++ src/Wt/Mail/Client.h
@@ -10,6 +10,8 @@
 #include <string>
 #include <Wt/WDllDefs.h>
 
+#include <memory>
+
 namespace Wt {
 
   /*! \brief Namespace for the \ref mail
@@ -21,6 +23,23 @@ class Message;
 /*! \defgroup mail SMTP client library (Wt::Mail)
  *  \brief An SMTP client implementation.
  */
+
+/*! \enum AuthenticationMethod
+ *  \brief Indicates the authentication method to use
+ */
+enum class AuthenticationMethod {
+  Plain, //!< PLAIN authentication
+  Login //!< LOGIN authentication
+};
+
+/*! \enum TransportEncryption
+ *  \brief Indicates the transport encryption method to use
+ */
+enum class TransportEncryption {
+  None, //!< No encryption
+  StartTLS, //!< Use STARTTLS (fail if not accepted by server)
+  TLS //!< Use TLS directly, don't use STARTTLS
+};
 
 /*! \class Client Wt/Mail/Client.h Wt/Mail/Client.h
  *  \brief An SMTP mail client.
@@ -77,6 +96,48 @@ public:
    */
   ~Client();
 
+  /*! \brief Enables authentication
+   */
+  void enableAuthentication(const std::string &username,
+                            const std::string &password,
+                            AuthenticationMethod method = AuthenticationMethod::Plain);
+
+  /*! \brief Returns the authentication method
+   *
+   * \sa enableAuthentication
+   */
+  AuthenticationMethod authenticationMethod() const { return configuration_.authenticationMethod_; }
+
+  /*! \brief Returns whether authentication is enabled
+   *
+   * \sa enableAuthentication
+   */
+  bool authenticationEnabled() const { return !configuration_.username_.empty() && !configuration_.password_.empty(); }
+
+  /*! \brief Sets whether SSL certificate verification is enabled
+   *
+   * Defaults to true
+   */
+  void setSslCertificateVerificationEnabled(bool enabled);
+
+  /*! \brief Gets whether SSL certificate verification is enabled
+   *
+   * \sa setSslCertificateVerificationEnabled
+   */
+  bool isSslCertificateVerificationEnabled() const { return configuration_.certificateVerificationEnabled_; }
+
+  /*! \brief Sets the transport encryption method
+   *
+   * The default is TransportEncryption::None
+   */
+  void setTransportEncryption(TransportEncryption method);
+
+  /*! \brief Get the encryption method
+   *
+   * \sa setTransportEncryption
+   */
+  TransportEncryption transportEncryption() const { return configuration_.transportEncryption_; }
+
   /*! \brief Connects to the default SMTP server.
    *
    * This connects to the SMTP server defined by the "smtp-host"
@@ -111,10 +172,22 @@ public:
 
 private:
   Client(const Client&);
+  class BaseImpl;
+  template<bool ssl>
   class Impl;
 
-  Impl *impl_;
-  std::string selfHost_;
+  std::unique_ptr<BaseImpl> impl_;
+
+  struct Configuration {
+    std::string selfHost_;
+    std::string username_;
+    std::string password_;
+    AuthenticationMethod authenticationMethod_;
+    TransportEncryption transportEncryption_;
+    bool certificateVerificationEnabled_;
+  };
+
+  Configuration configuration_;
 };
 
   }
