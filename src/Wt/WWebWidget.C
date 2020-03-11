3259a1a35fbec283b3294c2870a6b8e79639b94d
Roel Standaert <roel@emweb.be>
2020-03-11 16:59:11 +0100

    Fix use after free caused by order of destruction in WWebWidget destructor

--- src/Wt/WWebWidget.C
+++ src/Wt/WWebWidget.C
@@ -251,6 +251,14 @@ WWebWidget::~WWebWidget()
   // removeFromParent should always return a nullptr if it is called in the destructor!
   assert(unique_this == nullptr);
 #endif // WT_TARGET_JAVA
+
+  // Run destructors in this order (same as Wt 3), some destructors
+  // rely on otherImpl_ (loadToolTip_ JSignal destructor unexposes signal,
+  // which calls id(), which looks at otherImpl_)
+  transientImpl_.reset();
+  layoutImpl_.reset();
+  lookImpl_.reset();
+  otherImpl_.reset();
 }
 
 WCssDecorationStyle& WWebWidget::decorationStyle()
