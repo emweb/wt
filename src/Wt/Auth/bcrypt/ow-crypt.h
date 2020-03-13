9e5a4a9dfcfb635f38c1c0392dfa48dbd45f7489
Roel Standaert <roel@emweb.be>
2020-03-13 11:25:17 +0100

    Cherry-picking fixes from wt3 branch:
     - Fix issue #7482: lastId_ should be of type long long
     - Fix WCartesianChart panning with plain tooltips
       Ignore dragstart event on interactive WCartesianChart to avoid
       interference with panning implementation.
     - Issue #7480: prefix Wt's crypt functions

--- src/Wt/Auth/bcrypt/ow-crypt.h
+++ src/Wt/Auth/bcrypt/ow-crypt.h
@@ -22,22 +22,17 @@
 #define __const const
 #endif
 
-#ifndef __SKIP_GNU
-extern char *crypt(__const char *key, __const char *setting);
-extern char *crypt_r(__const char *key, __const char *setting, void *data);
-#endif
-
-#ifndef __SKIP_OW
-extern char *crypt_rn(__const char *key, __const char *setting,
+extern char *wt_crypt(__const char *key, __const char *setting);
+extern char *wt_crypt_r(__const char *key, __const char *setting, void *data);
+extern char *wt_crypt_rn(__const char *key, __const char *setting,
 	void *data, int size);
-extern char *crypt_ra(__const char *key, __const char *setting,
+extern char *wt_crypt_ra(__const char *key, __const char *setting,
 	void **data, int *size);
-extern char *crypt_gensalt(__const char *prefix, unsigned long count,
+extern char *wt_crypt_gensalt(__const char *prefix, unsigned long count,
 	__const char *input, int size);
-extern char *crypt_gensalt_rn(__const char *prefix, unsigned long count,
+extern char *wt_crypt_gensalt_rn(__const char *prefix, unsigned long count,
 	__const char *input, int size, char *output, int output_size);
-extern char *crypt_gensalt_ra(__const char *prefix, unsigned long count,
+extern char *wt_crypt_gensalt_ra(__const char *prefix, unsigned long count,
 	__const char *input, int size);
-#endif
 
 #endif
