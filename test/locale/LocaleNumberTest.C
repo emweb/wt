7a5e2b502b60306e5694331dd8da39a6571a2c34
Roel Standaert <roel@emweb.be>
2020-04-01 18:55:11 +0200

    These tests were failing with a recent version of MinGW
    Do the other test for MinGW, since libstdc++ doesn't insert that extra
    0.

--- test/locale/LocaleNumberTest.C
+++ test/locale/LocaleNumberTest.C
@@ -47,7 +47,7 @@ BOOST_AUTO_TEST_CASE( formatGrouping )
 
   s = l.toString(d * 1E30);
 
-#ifdef WT_WIN32
+#if defined(WT_WIN32) && !defined(__GNUC__)
   BOOST_REQUIRE(s == "1.23456782345e+037");
 #else
   BOOST_REQUIRE(s == "1.23456782345e+37");
@@ -59,7 +59,7 @@ BOOST_AUTO_TEST_CASE( formatGrouping )
 
   s = l.toString(d * -1E30);
 
-#ifdef WT_WIN32
+#if defined(WT_WIN32) && !defined(__GNUC__)
   BOOST_REQUIRE(s == "-1.23456782345e+037");
 #else
   BOOST_REQUIRE(s == "-1.23456782345e+37");
@@ -93,7 +93,7 @@ BOOST_AUTO_TEST_CASE( formatNegGrouping )
 
   s = l.toString(d * 1E30);
 
-#ifdef WT_WIN32
+#if defined(WT_WIN32) && !defined(__GNUC__)
   BOOST_REQUIRE(s == "-1.23456782345e+037");
 #else
   BOOST_REQUIRE(s == "-1.23456782345e+37");
@@ -105,7 +105,7 @@ BOOST_AUTO_TEST_CASE( formatNegGrouping )
 
   s = l.toString(d * -1E30);
 
-#ifdef WT_WIN32
+#if defined(WT_WIN32) && !defined(__GNUC__)
   BOOST_REQUIRE(s == "1.23456782345e+037");
 #else
   BOOST_REQUIRE(s == "1.23456782345e+37");
