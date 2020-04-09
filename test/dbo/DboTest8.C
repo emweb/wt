3f76c8407da3d42f506c21137fce9cc33a6eacd1
Roel Standaert <roel@emweb.be>
2020-04-09 15:22:57 +0200

    Integrated unit test for PR #161 into database test suite

--- test/dbo/DboTest8.C
+++ test/dbo/DboTest8.C
@@ -1,74 +1,85 @@
-#define BOOST_TEST_MODULE Foreign Key Constraint
+/*
+ * Copyright (C) 2020 Emweb bv, Herent, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+#include <boost/test/unit_test.hpp>
 
-#include <boost/test/included/unit_test.hpp>
-#include <Wt/Dbo/backend/Sqlite3.h>
 #include <Wt/Dbo/Dbo.h>
+
+#include "DboFixture.h"
+
 #include <algorithm>
 
 namespace dbo = Wt::Dbo;
 
 class TableSrc1
 {
-    public:
+public:
 
-        template<typename Action>
-        void persist([[maybe_unused]] Action &a)
-        {
-        }
+  template<typename Action>
+  void persist(Action &a)
+  {
+    (void)a;
+  }
 };
 
 class TableSrc2
 {
-    public:
+public:
 
-        template<typename Action>
-        void persist([[maybe_unused]] Action &a)
-        {
-        }
+  template<typename Action>
+  void persist(Action &a)
+  {
+    (void)a;
+  }
 };
 
 class TableDst
 {
-    public:
-        dbo::ptr<TableSrc1> table_src1;
-        dbo::ptr<TableSrc2> table_src2;
+public:
+  dbo::ptr<TableSrc1> table_src1;
+  dbo::ptr<TableSrc2> table_src2;
 
-        template<typename Action>
-        void persist(Action &a)
-        {
-            dbo::belongsTo(a, table_src1, dbo::OnDeleteRestrict);
-            dbo::belongsTo(a, table_src2, dbo::OnUpdateRestrict);
-        }
+  template<typename Action>
+  void persist(Action &a)
+  {
+    dbo::belongsTo(a, table_src1, dbo::OnDeleteRestrict);
+    dbo::belongsTo(a, table_src2, dbo::OnUpdateRestrict);
+  }
 };
 
-BOOST_AUTO_TEST_CASE(foreign_key_constraints_test)
+struct Dbo8Fixture : DboFixtureBase {
+  Dbo8Fixture()
+  {
+    session_->mapClass<TableSrc1>("table_src1");
+    session_->mapClass<TableSrc2>("table_src2");
+    session_->mapClass<TableDst>("table_dst");
+  }
+};
+
+BOOST_AUTO_TEST_CASE( dbo8_test1 )
 {
-    dbo::Session session;
-    auto conn = std::make_unique<dbo::backend::Sqlite3>(":memory:");
-    session.setConnection(std::move(conn));
+  Dbo8Fixture f;
+  dbo::Session &session = *f.session_;
 
-    session.mapClass<TableSrc1>("table_src1");
-    session.mapClass<TableSrc2>("table_src2");
-    session.mapClass<TableDst>("table_dst");
+  auto sql = session.tableCreationSql();
 
-    auto sql = session.tableCreationSql();
+  std::string needle_del_restrict = "on delete restrict";
+  const auto it_del_restrict = std::search(begin(sql), end(sql), begin(needle_del_restrict), end(needle_del_restrict));
+  BOOST_REQUIRE((it_del_restrict != end(sql)));
 
-    std::string needle_del_restrict = "on delete restrict";
-    const auto it_del_restrict = std::search(begin(sql), end(sql), begin(needle_del_restrict), end(needle_del_restrict));
-    BOOST_REQUIRE((it_del_restrict != cend(sql)));
+  std::string needle_update_restrict = "on update restrict";
+  const auto it_update_restrict = std::search(begin(sql), end(sql), begin(needle_update_restrict), end(needle_update_restrict));
+  BOOST_REQUIRE((it_update_restrict != end(sql)));
 
-    std::string needle_update_restrict = "on update restrict";
-    const auto it_update_restrict = std::search(begin(sql), end(sql), begin(needle_update_restrict), end(needle_update_restrict));
-    BOOST_REQUIRE((it_update_restrict != cend(sql)));
-
-    try
-    {
-        session.createTables();
-        BOOST_REQUIRE(true);
-    }
-    catch(...)
-    {
-        BOOST_REQUIRE(false);
-    }
-
+  try
+  {
+    session.createTables();
+    BOOST_REQUIRE(true);
+  }
+  catch(...)
+  {
+    BOOST_REQUIRE(false);
+  }
 }
