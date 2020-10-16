c7c6ef273efe44217dfa1c49d68223bacb54d39b
Roel Standaert <roel@emweb.be>
2020-10-16 14:15:14 +0200

    Improve handling of bad websocket max window bits
    Return error during websocket negotiation if a bad value for
    server_max_window_bits or client_max_window_bits is encountered.
    
    Added error logging for failed per_message_deflate negotiation.
    
    Using Wt::Utils::stoi instead of lexical_cast.
    
    Backported to wt3
    
    Co-authored-by: Bruce Toll <btoll@dhsus.com>

--- src/http/RequestParser.C
+++ src/http/RequestParser.C
@@ -359,9 +359,14 @@ bool RequestParser::doWebSocketPerMessageDeflateNegotiation(const Request& req, 
 		boost::trim(key);
 		size_t pos = key.find("=");
 		if (pos != std::string::npos) {
-		  hasServerWBit = true; 
-		  int ws = boost::lexical_cast<int>(key.substr(pos + 1));
-		  
+		  hasServerWBit = true;
+		  int ws = 0;
+		  try {
+		    ws = boost::lexical_cast<int>(key.substr(pos + 1));
+		  } catch (const boost::bad_lexical_cast &e) {
+		    return false;
+		  }
+
 		  if (ws < 8 || ws > 15) return false;
 
 		  req.pmdState_.server_max_window_bits  = ws;
@@ -374,9 +379,14 @@ bool RequestParser::doWebSocketPerMessageDeflateNegotiation(const Request& req, 
 		boost::trim(key);
 		size_t pos = key.find("=");
 		if (pos != std::string::npos) {
-		  hasClientWBit = true; 
-		  int ws = boost::lexical_cast<int>(key.substr(pos + 1));
-		  
+		  hasClientWBit = true;
+		  int ws = 0;
+		  try {
+		    ws = boost::lexical_cast<int>(key.substr(pos + 1));
+		  } catch (const boost::bad_lexical_cast &e) {
+		    return false;
+		  }
+
 		  if (ws < 8 || ws > 15) return false;
 
 		  req.pmdState_.client_max_window_bits = ws;
@@ -454,9 +464,11 @@ RequestParser::parseWebSocketMessage(Request& req, ReplyPtr reply,
 	  reply->addHeader("Sec-WebSocket-Accept", accept);
 #ifdef WTHTTP_WITH_ZLIB
 	  std::string compressHeader;
-	  if(!doWebSocketPerMessageDeflateNegotiation(req, compressHeader))
-		return Request::Error;
-	  
+	  if(!doWebSocketPerMessageDeflateNegotiation(req, compressHeader)) {
+	    LOG_ERROR("ws: error during per_message_deflate negotiation");
+	    return Request::Error;
+	  }
+
 	  if(!compressHeader.empty())  {
 		// We can use per message deflate
 		if(initInflate()) {
