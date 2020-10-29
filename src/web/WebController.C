e0fc9ed41eb47d292474b6e7e4fb28665d999b69
Roel Standaert <roel@emweb.be>
2020-10-29 12:56:43 +0100

    Readded parsing in requestDataReceived, because we do need that
    This partially reverts commit bac835fb6cbccf5b9e74d84d19259c2eabe90679
    
    When handleRequest was not called yet, but requestDataReceived was,
    the handling of the resource progress would already rely on the
    parameters, e.g.  to decide which session and resource the request
    belongs to.

--- src/web/WebController.C
+++ src/web/WebController.C
@@ -452,6 +452,15 @@ bool WebController::requestDataReceived(WebRequest *request,
     lock.unlock();
 #endif // WT_THREADED
 
+    CgiParser cgi(conf_.maxRequestSize(), conf_.maxFormDataSize());
+
+    try {
+      cgi.parse(*request, CgiParser::ReadHeadersOnly);
+    } catch (std::exception& e) {
+      LOG_ERROR_S(&server_, "could not parse request: " << e.what());
+      return false;
+    }
+
     const std::string *wtdE = request->getParameter("wtd");
     if (!wtdE)
       return false;
