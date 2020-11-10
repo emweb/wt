b23cc9f89746943ba8854235eeb28959ca6d67af
Roel Standaert <roel@emweb.be>
2020-11-10 13:57:47 +0100

    WMediaPlayer: fixes for reload-is-new-session = false
    When refreshing the page the JavaScript for jplayer was
    loaded before jquery, causing JS errors. The jplayer JS
    is loaded first because any JS file that is require()d
    is inserted as a <script> tag when progressive bootstrap
    is used, or when reload-is-new-session = false, and
    the page is refreshed.
    
    This change makes WMediaPlayer use requireJQuery, which
    makes it load jquery just like any other JS file. This
    is however not applicable if the user has already set
    another version of jquery.
    
    Also, on refresh the media source has to be set again,
    hence the flags.test(RenderFlag::Full) && !media_.empty()

--- src/Wt/WMediaPlayer.C
+++ src/Wt/WMediaPlayer.C
@@ -127,8 +127,8 @@ WMediaPlayer::WMediaPlayer(MediaType mediaType)
 
   std::string res = WApplication::relativeResourcesUrl() + "jPlayer/";
 
-  if (!app->environment().ajax())
-    app->require(res + "jquery.min.js");
+  if (!app->customJQuery())
+    app->requireJQuery(res + "jquery.min.js");
 
   if (app->require(res + "jquery.jplayer.min.js"))
     app->useStyleSheet(res + "skin/jplayer.blue.monday.css");
@@ -446,7 +446,8 @@ void WMediaPlayer::render(WFlags<RenderFlag> flags)
 
   WApplication *app = WApplication::instance();
 
-  if (mediaUpdated_) {
+  if (mediaUpdated_ ||
+      (flags.test(RenderFlag::Full) && !media_.empty())) {
     WStringStream ss;
 
     ss << '{';
