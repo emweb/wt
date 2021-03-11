ccc24ee662ac3c9cc17e0cc077903f04bf9374fc
Korneel Dumon <korneel@emweb.be>
2021-03-11 17:22:43 +0100

    WT-8194: cannot pan WVirtualImage when it gets too large
    if you use mouseMoved() signal of images, a handler gets added automatically
    for onmousedown. This overrides the attribute that is set in the c++ code
    and the images become draggable again.

--- src/Wt/WVirtualImage.C
+++ src/Wt/WVirtualImage.C
@@ -189,7 +189,7 @@ void WVirtualImage::generateGridItems(::int64_t newX, ::int64_t newY)
 	      std::unique_ptr<WImage> img
 	        = createImage(i * gridImageSize_, j * gridImageSize_, width, height);
 
-	      img->setAttributeValue("onmousedown", "return false;");
+	      img->setAttributeValue("draggable", "false");
 	      img->setPositionScheme(PositionScheme::Absolute);
 	      img->setOffsets((double)i * gridImageSize_, Side::Left);
 	      img->setOffsets((double)j * gridImageSize_, Side::Top);
