48d0e912afcf92e233cefba63a3c3cb02e89ac8a
Yoika Ghysens <yoika@emweb.be>
2021-08-25 16:51:41 +0200

    WT-8622: Complete Dbo Forms documentation
    Documentation for the Dbo Forms was already written when it was
    first implemented, but it seems I missed a few things. This commit
    focuses on completing the existing documentation and adding some
    for parts that were missed.
    
    Note that I used `\internal` in some places. When the INTERNAL_DOCS
    configuration option is set to "NO" these sections will be omitted
    from the generated documentation.

--- src/Wt/Form/Dbo/FormModelBase.h
+++ src/Wt/Form/Dbo/FormModelBase.h
@@ -12,25 +12,33 @@
 namespace Wt {
   namespace Form {
     namespace Dbo {
-/*! \brief A model class keeping information about database columns
- * that are to be used in the model/view logic.
+/*! \class FormModelBase Wt/Form/Dbo/FormModelBase.h Wt/Form/Dbo/FormModelBase.h
+ *  \brief A model class keeping information about database columns that are to be used in the model/view logic.
+ *
+ * \sa FormModel, FormView
+ *
+ * \ingroup form
  */
 class FormModelBase : public Wt::WFormModel
 {
 public:
+  /*! \brief Constructor
+   *
+   * \sa WFormModel
+   */
   FormModelBase()
     : Wt::WFormModel()
   {
   }
 
-  /*! \brief Returns the Dbo columns used in this model
+  /*! \brief Returns the %Dbo columns used in this model
    */
   const std::vector<std::string>& dboFields() const { return dboFields_; }
 
 protected:
   std::vector<std::string> dboFields_;
 
-  /*! \brief Adds a Dbo column as a field
+  /*! \brief Adds a %Dbo column as a field
    */
   void insertDboField(Wt::WFormModel::Field field)
   {
