6b04078922b84dd96f51a138b098b7d6c22c3445
Roel Standaert <roel@emweb.be>
2022-08-12 12:18:54 +0200

    WT-8629: Added configuration for new minifier
    Most popular minifiers etc. are node-based these days, so we'll use the
    rather popular terser to minify our JavaScript, and script it with gulp.
    
    This replaces the old version of Google Closure compiler we used to use.

---
+++ src/js/gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const terser = require('gulp-terser');
+
+const terserOptions = {
+  compress: {
+    // Turning semicolons into commas breaks Wt's JavaScript as C++ code macro trick
+    // It turns two statements like:
+    //   WT_DECLARE_WT_MEMBER(...);
+    //   WT_DECLARE_WT_MEMBER(...);
+    // into:
+    //   WT_DECLARE_WT_MEMBER(...),WT_DECLARE_WT_MEMBER(...)
+    sequences: false
+  },
+  format: {
+    comments: 'some',
+    ecma: '2015'
+  }
+};
+
+const renameOptions = {
+  extname: '.min.js'
+};
+
+gulp.task('js', function(done) {
+  gulp.src([
+    '*.js',
+    '!gulpfile.js',
+    '!*.min.js'
+  ])
+    .pipe(terser(terserOptions))
+    .pipe(rename(renameOptions))
+    .pipe(gulp.dest('.'))
+    .on('end', done);
+});
+
+gulp.task('skeleton', function(done) {
+  gulp.src([
+    '../web/skeleton/Boot.js',
+    '../web/skeleton/Wt.js',
+  ])
+    .pipe(terser(terserOptions))
+    .pipe(rename(renameOptions))
+    .pipe(gulp.dest('../web/skeleton'))
+    .on('end', done);
+});
+
+gulp.task('default', gulp.series('js', 'skeleton'));
