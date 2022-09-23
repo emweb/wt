9cadff23e0a787961408083b5954ac4467437ed1
Natan Pirard <natan@legion.leuven.emweb.be>
2022-09-23 15:24:35 +0200

    WT-10967: Fix eslint warnings in WTimeValidator.js
    - replace `var` by `let` or `const`
    - replace == by ===
      - made sure results is either null or an array,
        so that the === null check works properly
    - Use for...of loop over formats
    
    Co-authored-by: Roel Standaert <roel@emweb.be>

--- src/js/WTimeValidator.js
+++ src/js/WTimeValidator.js
@@ -4,24 +4,24 @@ WT_DECLARE_WT_MEMBER(
   "WTimeValidator",
   function(mandatory, formats, bottom, top, blankError, formatError, tooSmallError, tooLargeError) {
     this.validate = function(text) {
-      if (text.length == 0) {
+      if (text.length === 0) {
         if (mandatory) {
           return { valid: false, message: blankError };
         } else {
           return { valid: true };
         }
       }
-      var results;
-      var h = -1, m = -1, s = -1, ms = -1;
-      for (var i = 0; i < formats.length; ++i) {
-        var f = formats[i];
-        var r = new RegExp("^" + f.regexp + "$");
+      /** @type {?[string]} */
+      let results = null;
+      let h = -1, m = -1, s = -1, ms = -1;
+      for (const f of formats) {
+        const r = new RegExp("^" + f.regexp + "$");
         results = r.exec(text);
-        if (results != null) {
+        if (results !== null) {
           h = f.getHour(results);
           if (text.toUpperCase().indexOf("P") > -1 && h < 12) {
             h += 12;
-          } else if (text.toUpperCase().indexOf("A") > -1 && h == 12) {
+          } else if (text.toUpperCase().indexOf("A") > -1 && h === 12) {
             h = 0;
           }
           m = f.getMinutes(results);
@@ -30,17 +30,17 @@ WT_DECLARE_WT_MEMBER(
           break;
         }
       }
-      if (results == null) {
+      if (results === null) {
         return { valid: false, message: formatError };
       }
       if ((h < 0) || (h > 23) || (m < 0) || (m > 59) || (s < 0) || (s > 59) || (ms < 0) || (ms > 999)) {
         return { valid: false, message: formatError };
       }
 
-      var dt = new Date(0, 0, 0, h, m, s, ms);
+      const dt = new Date(0, 0, 0, h, m, s, ms);
       if (
-        dt.getHours() != h || dt.getMinutes() != m ||
-        dt.getSeconds() != s || dt.getMilliseconds() != ms
+        dt.getHours() !== h || dt.getMinutes() !== m ||
+        dt.getSeconds() !== s || dt.getMilliseconds() !== ms
       ) {
         return { valid: false, message: formatError };
       }
