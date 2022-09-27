5e9499a8f8e6218ee6a4fb262ec342572e977ee5
Natan Pirard <natan@legion.leuven.emweb.be>
2022-09-27 14:51:41 +0200

    WT-10967: Fix eslint warnings in WPopupMenu.js
    - replace 'var' by 'let' or 'const'
    - replace == by ===
    - remove unused variables
    - replaced indexed for loop with for...of loop
    
    Co-authored-by: Roel Standaert <roel@emweb.be>

--- src/js/WPopupMenu.js
+++ src/js/WPopupMenu.js
@@ -9,13 +9,10 @@
 WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, autoHideDelay) {
   el.wtObj = this;
 
-  var self = this,
-    WT = APP.WT,
-    hideTimeout = null,
-    entered = false,
+  const WT = APP.WT;
+  let hideTimeout = null,
     current = null,
-    touch = null,
-    haveMouseDown = false;
+    touch = null;
 
   if (WT.isIOS) {
     el.addEventListener("touchstart", startElTouch);
@@ -38,7 +35,7 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
     if (item.subMenu) {
       return item.subMenu;
     } else {
-      var u = item.lastChild;
+      const u = item.lastChild;
       if (u && WT.hasTag(u, "UL")) {
         item.subMenu = u;
         u.parentItem = item;
@@ -56,7 +53,7 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
 
   function showSubmenu(menu) {
     menu.style.display = "block";
-    if (menu.parentNode == menu.parentItem) {
+    if (menu.parentNode === menu.parentItem) {
       menu.parentNode.removeChild(menu);
       el.parentNode.appendChild(menu);
     }
@@ -64,7 +61,7 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
       * we actually want to align the first item, so we need to adjust
       * for the menu padding and border
       */
-    var margin = WT.px(menu, "paddingTop") + WT.px(menu, "borderTopWidth");
+    const margin = WT.px(menu, "paddingTop") + WT.px(menu, "borderTopWidth");
     WT.positionAtWidget(menu.id, menu.parentItem.id, WT.Horizontal, -margin);
     setOthersInactive(menu, null);
 
@@ -78,10 +75,10 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
 
   function setOthersInactive(topLevelMenu, activeItem) {
     function itemLeadsTo(item1, item2) {
-      if (item1 == item2) {
+      if (item1 === item2) {
         return true;
       } else if (item2) {
-        var parent = item2.parentNode.parentItem;
+        const parent = item2.parentNode.parentItem;
         if (parent) {
           return itemLeadsTo(item1, parent);
         } else {
@@ -93,20 +90,16 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
     }
 
     function processMenu(menu) {
-      var i, il;
-
-      for (i = 0, il = menu.childNodes.length; i < il; ++i) {
-        var item = menu.childNodes[i];
-
+      for (const item of menu.childNodes) {
         if (!itemLeadsTo(item, activeItem)) {
           setActive(item, false);
-          var sm = submenu(item);
+          const sm = submenu(item);
           if (sm) {
             sm.style.display = "none";
             processMenu(sm);
           }
         } else if (item !== activeItem) {
-          var sm = submenu(item);
+          const sm = submenu(item);
           if (sm) {
             processMenu(sm);
           }
@@ -122,7 +115,7 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
       * If mouse is over an item that has a sub menu, then show the sub menu
       * if it is not already shown
       */
-    var item = WT.target(e);
+    let item = WT.target(e);
 
     while (item && !WT.hasTag(item, "LI") && !WT.hasTag(item, "UL")) {
       item = item.parentNode;
@@ -137,7 +130,7 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
 
       setActive(item, true);
 
-      var menu = submenu(item);
+      const menu = submenu(item);
       if (menu) {
         showSubmenu(menu);
       }
@@ -150,7 +143,6 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
   }
 
   function mouseLeave() {
-    entered = false;
     clearTimeout(hideTimeout);
     if (autoHideDelay >= 0) {
       hideTimeout = setTimeout(doHide, autoHideDelay);
@@ -158,7 +150,6 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
   }
 
   function mouseEnter() {
-    entered = true;
     clearTimeout(hideTimeout);
   }
 
@@ -168,40 +159,33 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
   }
 
   function stillExist() {
-    return document.getElementById(el.id) != null;
+    return document.getElementById(el.id) !== null;
   }
 
   function onDocumentDown(event) {
-    haveMouseDown = true;
-    if (stillExist() && WT.button(event) != 1) {
+    if (stillExist() && WT.button(event) !== 1) {
       doHide();
     }
   }
 
-  function onDocumentClick(event) {
+  function onDocumentClick() {
     if (stillExist()) {
-      haveMouseDown = false;
       doHide();
     }
   }
 
   function onDocumentKeyDown(event) {
-    if (stillExist() && event.keyCode == 27) {
+    if (stillExist() && event.keyCode === 27) {
       doHide();
     }
   }
 
   this.setHidden = function(hidden) {
-    if (!hidden) {
-      haveMouseDown = false; // cancel pending hide
-    }
-
     if (hideTimeout) {
       clearTimeout(hideTimeout);
       hideTimeout = null;
     }
 
-    entered = false;
     current = null;
 
     if (hidden) {
@@ -247,7 +231,7 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
   }
 
   function startTouch(event) {
-    var l = event.originalEvent.touches;
+    const l = event.originalEvent.touches;
     if (l.length > 1) {
       touch = null;
     } else {
@@ -264,7 +248,7 @@ WT_DECLARE_WT_MEMBER(1, JavaScriptConstructor, "WPopupMenu", function(APP, el, a
 
   function endTouch(event) {
     if (touch) {
-      var t = event.originalEvent.changedTouches[0];
+      const t = event.originalEvent.changedTouches[0];
       if (Math.abs(touch.x - t.screenX) < 20 && Math.abs(touch.y - t.screenY) < 20) {
         onDocumentClick(event);
       }
