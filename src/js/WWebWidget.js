4ba8874c397ddbcdb2df664e5583a51b560e5e1c
Roel Standaert <roel@emweb.be>
2022-11-09 16:46:36 +0100

    WT-11067: overhauling animateDisplay to make animations smoother
    The animations were often quite jittery, and since modern browsers
    have a proper JavaScript animation API with keyframes, I opted to
    use that instead of the existing implementation, which relied on
    adding and removing CSS classes at the appropriate time.
    
    A lot of JSDoc type annotations were added to aid in development.

--- src/js/WWebWidget.js
+++ src/js/WWebWidget.js
@@ -6,313 +6,365 @@
 
 /* Note: this is at the same time valid JavaScript and C++. */
 
-WT_DECLARE_WT_MEMBER(1, JavaScriptFunction, "animateDisplay", function(APP, id, effects, timing, duration, display) {
-  const WT = APP.WT;
+WT_DECLARE_WT_MEMBER(
+  1,
+  JavaScriptFunction,
+  "animateDisplay", /**
+   * @param {Object} APP
+   * @param {Object} APP.WT
+   * @param {Object} APP.layouts2
+   * @param {string} id
+   * @param {number} effects
+   * @param {number} timing
+   * @param {number} duration
+   * @param {string} display
+   */
+  function(APP, id, effects, timing, duration, display) {
+    const WT = APP.WT;
 
-  const doAnimateDisplay = function(id, effects, timing, duration, display) {
-    const NoEffect = 0x0;
-    const SlideInFromLeft = 0x1;
-    const SlideInFromRight = 0x2;
-    const SlideInFromBottom = 0x3;
-    const SlideInFromTop = 0x4;
-    const Pop = 0x5;
-    const Fade = 0x100;
+    /**
+     * @param {string} id
+     * @param {number} effects
+     * @param {number} timing
+     * @param {number} duration
+     * @param {string} display
+     */
+    function doAnimateDisplay(id, effects, timing, duration, display) {
+      const Effect = {
+        None: 0x0,
+        SlideInFromLeft: 0x1,
+        SlideInFromRight: 0x2,
+        SlideInFromBottom: 0x3,
+        SlideInFromTop: 0x4,
+        Pop: 0x5,
+        Fade: 0x100,
+      };
 
-    // const Ease = 0;
-    // const Linear = 1;
-    // const EaseIn = 2;
-    // const EaseOut = 3;
-    // const EaseInOut = 4;
-    // const CubicBezier = 5;
+      const Timing = {
+        Ease: 0,
+        Linear: 1,
+        EaseIn: 2,
+        EaseOut: 3,
+        EaseInOut: 4,
+        CubicBezier: 5,
+      };
 
-    const timings = ["ease", "linear", "ease-in", "ease-out", "ease-in-out"],
-      inverseTiming = [0, 1, 3, 2, 4, 5];
+      const timings = [
+        "ease",
+        "linear",
+        "ease-in",
+        "ease-out",
+        "ease-in-out",
+      ];
 
-    const animationPrefix = WT.vendorPrefix(WT.styleAttribute("animation"));
-    const transitionPrefix = WT.vendorPrefix(WT.styleAttribute("transition"));
-    const transformPrefix = WT.vendorPrefix(WT.styleAttribute("transform"));
-    const el = WT.$(id),
-      animationEventEnd = animationPrefix === "Webkit" ?
-        "webkitAnimationEnd" :
-        "animationend",
-      transitionEventEnd = transitionPrefix === "Webkit" ?
-        "webkitTransitionEnd" :
-        "transitionend";
+      const inverseTiming = [
+        Timing.Ease,
+        Timing.Linear,
+        Timing.EaseOut,
+        Timing.EaseIn,
+        Timing.EaseInOut,
+        Timing.CubicBezier,
+      ];
 
-    if (WT.css(el, "display") !== display) {
-      const p = el.parentNode;
+      const el = document.getElementById(id);
+      const elc = el.childElementCount === 1 ? el.firstElementChild : null;
 
-      if (p.wtAnimateChild) {
-        p.wtAnimateChild(WT, el, effects, timing, duration, { display: display });
-        return;
-      }
-
-      if (el.classList.contains("animating")) {
-        el.addEventListener(transitionEventEnd, function() {
-          doAnimateDisplay(id, effects, timing, duration, display);
-        }, { once: true });
-        return;
-      }
-
-      el.classList.add("animating");
-
-      const effect = effects & 0xFF,
-        hide = display === "none",
-        cssTiming = timings[hide ? inverseTiming[timing] : timing],
-        elStyle = {};
-
+      /**
+       * @param {HTMLElement} el
+       * @param {Object} style
+       * @param {Object} [savedStyle]
+       */
       function set(el, style, savedStyle) {
         for (const i of Object.keys(style)) {
-          let k = i;
-
-          if (k === "animationDuration" && animationPrefix !== "") {
-            k = animationPrefix + k.substring(0, 1).toUpperCase() +
-              k.substring(1);
-          } else if (k === "transform" && transformPrefix !== "") {
-            k = transformPrefix + k.substring(0, 1).toUpperCase() +
-              k.substring(1);
-          } else if (k === "transition" && transitionPrefix !== "") {
-            k = transitionPrefix + k.substring(0, 1).toUpperCase() +
-              k.substring(1);
+          if (savedStyle && typeof savedStyle[i] === "undefined") {
+            savedStyle[i] = el.style[i];
           }
-
-          if (savedStyle && typeof (savedStyle[k]) === "undefined") {
-            savedStyle[k] = el.style[k];
-          }
-          el.style[k] = style[i];
+          el.style[i] = style[i];
         }
       }
 
       const restore = set;
 
-      function onEnd() {
-        if (el.wtAnimatedHidden) {
-          el.wtAnimatedHidden(hide);
+      const displayChanged = (() => {
+        // Experimentally set display to the given display option, so
+        // we can check if the display value actually changed.
+        const elStyle = {};
+        const prevDisplay = WT.css(el, "display");
+        set(el, { display }, elStyle);
+        const newDisplay = WT.css(el, "display");
+        restore(el, elStyle);
+        return prevDisplay !== newDisplay;
+      })();
+
+      if (displayChanged) {
+        const p = el.parentNode;
+
+        if (p.wtAnimateChild) {
+          p.wtAnimateChild(WT, el, effects, timing, duration, { display });
+          return;
         }
 
-        el.classList.remove("animating");
-
-        if (APP.layouts2) {
-          APP.layouts2.setElementDirty(el);
+        if (el.classList.contains("animating")) {
+          // already animating, wait until animation is done
+          Promise.all(
+            el.getAnimations()
+              .map((animation) => animation.finished)
+          ).then(() => {
+            doAnimateDisplay(id, effects, timing, duration, display);
+          });
+          return;
         }
-      }
 
-      function show() {
-        el.style.display = display;
-        if (el.wtPosition) {
-          el.wtPosition();
+        el.classList.add("animating");
+
+        const effect = effects & 0xFF,
+          hide = display === "none",
+          easing = timings[hide ? inverseTiming[timing] : timing],
+          elStyle = {},
+          elcStyle = {};
+
+        const animationOptions = {
+          duration,
+          iterations: 1,
+          easing,
+        };
+
+        function onEnd() {
+          if (el.wtAnimatedHidden) {
+            el.wtAnimatedHidden(hide);
+          }
+
+          if (el.getAnimations().length === 0) {
+            el.classList.remove("animating");
+          }
+
+          if (APP.layouts2) {
+            APP.layouts2.setElementDirty(el);
+          }
         }
-        if (window.onshow) {
-          window.onshow();
+
+        function show() {
+          el.style.display = display;
+          if (el.wtPosition) {
+            el.wtPosition();
+          }
+          if (window.onshow) {
+            window.onshow();
+          }
         }
-      }
 
-      function animateStaticVertical() {
-        let targetHeight, currentHeight;
-        const elcStyle = {};
-        let elc;
+        function animateStaticVertical() {
+          if (!hide) {
+            show();
+          }
 
-        if (hide) {
-          currentHeight = WT.css(el, "height");
-          set(el, { height: currentHeight, overflow: "hidden" }, elStyle);
+          set(el, { overflow: "hidden" }, elStyle);
 
-          if (effect === SlideInFromTop && el.childNodes.length === 1) {
-            elc = el.firstChild;
-            set(elc, { transform: "translateY(0)" }, elcStyle);
-            if (!WT.hasTag(elc, "TABLE")) {
-              set(elc, { display: "block" }, elcStyle);
+          const animations = [];
+
+          const elcPos = (() => {
+            if (!elc) {
+              return null;
             }
+            const elRect = el.getBoundingClientRect();
+            const elcRect = elc.getBoundingClientRect();
+            return {
+              left: elcRect.x - elRect.x - WT.px(el, "border-left-width"),
+              top: elcRect.y - elRect.y - WT.px(el, "border-top-width"),
+            };
+          })();
+
+          if (elc && effect === Effect.SlideInFromTop) {
+            // Content slides along
+            const visibleKeyframe = {
+              transform: "translateY(0px)",
+            };
+            const hiddenKeyframe = {
+              transform: `translateY(-${el.clientHeight}px)`,
+            };
+            const keyframes = hide ? [visibleKeyframe, hiddenKeyframe] : [hiddenKeyframe, visibleKeyframe];
+            animations.push(elc.animate(keyframes, animationOptions));
           }
 
-          targetHeight = "0px";
-        } else {
-          const pStyle = {};
+          {
+            const hiddenKeyframe = {
+              height: "0px",
+              paddingTop: "0px",
+              paddingBottom: "0px",
+              borderTopWidth: "0px",
+              borderBottomWidth: "0px",
+            };
+            const visibleKeyframe = {
+              height: WT.css(el, "height"),
+              paddingTop: WT.css(el, "padding-top"),
+              paddingBottom: WT.css(el, "padding-bottom"),
+              borderTopWidth: WT.css(el, "border-top-width"),
+              borderBottomWidth: WT.css(el, "border-bottom-width"),
+            };
 
-          set(p, { height: WT.css(p, "height"), overflow: "hidden" }, pStyle);
+            if (effects & Effect.Fade) {
+              hiddenKeyframe.opacity = 0;
+              visibleKeyframe.opacity = 1;
+            }
 
-          show();
-
-          if (WT.css(el, "height") === "0px") {
-            el.style.height = "auto";
-          }
-
-          targetHeight = WT.css(el, "height");
-          set(el, { height: "0px", overflow: "hidden" }, elStyle);
-          restore(p, pStyle);
-
-          if (effect === SlideInFromTop) {
-            set(el, { WebkitBackfaceVisibility: "visible" }, elStyle);
-            el.scrollTop = 1000;
-          }
-        }
-
-        if (effects & Fade) {
-          set(el, { opacity: (hide ? 1 : 0) }, elStyle);
-        }
-
-        currentHeight = el.clientHeight; // force 'absorbing' set height
-
-        setTimeout(function() {
-          set(el, { transition: "all " + duration + "ms " + cssTiming, height: targetHeight }, elStyle);
-
-          if (effects & Fade) {
-            set(el, { opacity: (hide ? 0 : 1) });
+            const keyframes = hide ? [visibleKeyframe, hiddenKeyframe] : [hiddenKeyframe, visibleKeyframe];
+            animations.push(el.animate(keyframes, animationOptions));
           }
 
           if (elc) {
-            set(elc, {
-              transition: "all " + duration + "ms " + cssTiming,
-              transform: "translateY(-" + currentHeight + ")",
-            }, elcStyle);
+            // Fix content position
+            set(el, { position: "relative" }, elStyle);
+            const style = {
+              position: "absolute",
+              left: `${elcPos.left}px`,
+              top: `${elcPos.top}px`,
+            };
+            set(elc, style, elcStyle);
           }
 
-          el.addEventListener(transitionEventEnd, function() {
+          Promise.all(animations.map((animation) => animation.finished)).then(() => {
             if (hide) {
               el.style.display = display;
             }
 
             restore(el, elStyle);
-            if (effect === SlideInFromTop) {
-              el.scrollTop = 0;
-              if (elc) {
-                restore(elc, elcStyle);
-              }
-            }
+            restore(elc, elcStyle);
 
             onEnd();
-          }, { once: true });
-        }, 0);
-      }
-
-      function animateAbsolute(cssSize, cssOffset, topleft, U) {
-        if (!hide) {
-          show();
+          });
         }
 
-        const size = WT.px(el, cssSize),
-          hiddenU = (WT.px(el, cssOffset) + size) * (topleft ? -1 : 1);
-
-        let targetU;
-        if (hide) {
-          set(el, { transform: "translate" + U + "(0px)" }, elStyle);
-          targetU = hiddenU;
-        } else {
-          set(el, { transform: "translate" + U + "(" + hiddenU + "px)" }, elStyle);
-          targetU = 0;
-        }
-
-        if (effects & Fade) {
-          set(el, { opacity: (hide ? 1 : 0) }, elStyle);
-        }
-
-        setTimeout(function() {
-          set(el, {
-            transition: "all " + duration + "ms " + cssTiming,
-            transform: "translate" + U + "(" + targetU + "px)",
-          }, elStyle);
-
-          if (effects & Fade) {
-            set(el, { opacity: (hide ? 0 : 1) });
-          }
-
-          el.addEventListener(transitionEventEnd, function() {
-            if (hide) {
-              el.style.display = display;
-            }
-
-            restore(el, elStyle);
-
-            onEnd();
-          }, { once: true });
-        }, 50); // If this timeout is too small or 0, this will cause some browsers, like
-        // Chrome to sometimes not perform the animation at all.
-      }
-
-      function animateAbsoluteVertical() {
-        animateAbsolute("height", effect === SlideInFromTop ? "top" : "bottom", effect === SlideInFromTop, "Y");
-      }
-
-      function animateAbsoluteHorizontal() {
-        animateAbsolute("width", effect === SlideInFromLeft ? "left" : "right", effect === SlideInFromLeft, "X");
-      }
-
-      function animateTransition() {
-        set(el, { animationDuration: duration + "ms" }, elStyle);
-
-        if (hide) {
-          el.classList.remove("in");
-        }
-
-        let cl;
-
-        switch (effect) {
-          case Pop:
-            cl = "pop";
-            break;
-          case SlideInFromLeft:
-            cl = hide ? "slide" : "slide reverse";
-            break;
-          case SlideInFromRight:
-            cl = hide ? "slide reverse" : "slide";
-            break;
-        }
-
-        cl += hide ? " out" : " in";
-
-        if (effects & Fade) {
-          cl += " fade";
-        }
-
-        if (!hide) {
-          show();
-        }
-
-        el.classList.add(...cl.split(" ").filter((el) => el !== ""));
-        el.addEventListener(animationEventEnd, function() {
+        /**
+         * @param {string} cssSize
+         * @param {string} cssOffset
+         * @param {boolean} topleft
+         * @param {string} U
+         */
+        function animateAbsolute(cssSize, cssOffset, topleft, U) {
           if (!hide) {
-            cl = cl.replace(" in", "");
+            show();
           }
-          el.classList.remove(...cl.split(" ").filter((el) => el !== ""));
-          if (hide) {
-            el.style.display = display;
-          }
-          restore(el, elStyle);
-          onEnd();
-        }, { once: true });
-      }
 
-      setTimeout(function() {
-        const position = WT.css(el, "position"),
-          absolute = (position === "absolute" || position === "fixed");
+          const size = WT.px(el, cssSize),
+            hiddenU = (WT.px(el, cssOffset) + size) * (topleft ? -1 : 1);
+
+          const hiddenKeyframe = {
+            transform: `translate${U}(${hiddenU}px)`,
+          };
+          const visibleKeyframe = {
+            transform: `translate${U}(0px)`,
+          };
+
+          if (effects & Effect.Fade) {
+            hiddenKeyframe.opacity = 0;
+            visibleKeyframe.opacity = 1;
+          }
+
+          const keyframes = hide ? [visibleKeyframe, hiddenKeyframe] : [hiddenKeyframe, visibleKeyframe];
+          const animation = el.animate(keyframes, animationOptions);
+
+          animation.finished.then(() => {
+            if (hide) {
+              el.style.display = display;
+            }
+
+            onEnd();
+          });
+        }
+
+        function animateAbsoluteVertical() {
+          animateAbsolute(
+            "height",
+            effect === Effect.SlideInFromTop ? "top" : "bottom",
+            effect === Effect.SlideInFromTop,
+            "Y"
+          );
+        }
+
+        function animateAbsoluteHorizontal() {
+          animateAbsolute(
+            "width",
+            effect === Effect.SlideInFromLeft ? "left" : "right",
+            effect === Effect.SlideInFromLeft,
+            "X"
+          );
+        }
+
+        function animateTransition() {
+          if (!hide) {
+            show();
+          }
+
+          const hiddenKeyframe = {};
+          const visibleKeyframe = {};
+          switch (effect) {
+            case Effect.Pop:
+              set(el, { transformOrigin: "50% 50%" }, elStyle);
+              hiddenKeyframe.transform = "scale(.2)";
+              visibleKeyframe.transform = "scale(1)";
+              break;
+            case Effect.SlideInFromRight:
+              hiddenKeyframe.transform = "translateX(100%)";
+              // hiddenKeyframe.clipPath = 'polygon(-100% 0%, 0% 0%, 0% 100%, -100% 100%)';
+              visibleKeyframe.transform = "translateX(0)";
+              // visibleKeyframe.clipPath = 'polygon(-100% 0%, 100% 0%, 100% 100%, -100% 100%)';
+              break;
+            case Effect.SlideInFromLeft:
+              hiddenKeyframe.transform = "translateX(-100%)";
+              // hiddenKeyframe.clipPath = 'polygon(100% 0%, 200% 0%, 200% 100%, 100% 100%)';
+              visibleKeyframe.transform = "translateX(0)";
+              // visibleKeyframe.clipPath = 'polygon(0% 0%, 200% 0%, 200% 100%, 0% 100%)';
+              break;
+          }
+          if (effects & Effect.Fade) {
+            hiddenKeyframe.opacity = 0;
+            visibleKeyframe.opacity = WT.css(el, "opacity");
+          }
+
+          const keyframes = hide ? [visibleKeyframe, hiddenKeyframe] : [hiddenKeyframe, visibleKeyframe];
+          const animation = el.animate(keyframes, animationOptions);
+
+          animation.finished.then(() => {
+            if (hide) {
+              el.style.display = display;
+            }
+
+            restore(el, elStyle);
+
+            onEnd();
+          });
+        }
+
+        const absolute = ["absolute", "fixed"].includes(WT.css(el, "position"));
 
         switch (effect) {
-          case SlideInFromTop:
-          case SlideInFromBottom:
-            if (!absolute) {
-              animateStaticVertical();
-            } else {
+          case Effect.SlideInFromTop:
+          case Effect.SlideInFromBottom:
+            if (absolute) {
               animateAbsoluteVertical();
+            } else {
+              animateStaticVertical();
             }
             break;
-          case SlideInFromLeft:
-          case SlideInFromRight:
+          case Effect.SlideInFromLeft:
+          case Effect.SlideInFromRight:
             if (absolute) {
               animateAbsoluteHorizontal();
             } else {
               animateTransition();
             }
             break;
-          case NoEffect:
-          case Pop:
+          case Effect.None:
+          case Effect.Pop:
             animateTransition();
             break;
         }
-      }, 0);
+      }
     }
-  };
 
-  doAnimateDisplay(id, effects, timing, duration, display);
-});
+    doAnimateDisplay(id, effects, timing, duration, display);
+  }
+);
 
 WT_DECLARE_WT_MEMBER(
   2,
