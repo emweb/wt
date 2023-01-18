9af5fc1c38e5cfb141fb0be9363b75cbb407d3d7
Roel Standaert <roel@emweb.be>
2023-01-18 16:32:41 +0100

    WT-11262: remove dead code
    WT_HAVE_POSIX_FILEIO seems to be an old define that was maybe used at
    some point. Let's just always use boost::filesystem instead.

--- src/web/FileUtils.C
+++ src/web/FileUtils.C
@@ -6,13 +6,7 @@
 
 #include "web/FileUtils.h"
 
-#ifndef WT_HAVE_POSIX_FILEIO
 #include <boost/filesystem/operations.hpp>
-#else //WT_HAVE_POSIX_FILEIO
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-#endif //WT_HAVE_POSIX_FILEIO
 
 #include "web/WebUtils.h"
 #include "Wt/WException.h"
@@ -51,18 +45,7 @@ namespace Wt {
 
     unsigned long long size(const std::string &file)
     {
-#ifndef WT_HAVE_POSIX_FILEIO
       return (unsigned long long) boost::filesystem::file_size(file);
-#else //WT_HAVE_POSIX_FILEIO
-      struct stat sb;
-      if (stat(file.c_str(), &sb) == -1) {
-        std::string error
-          = "size: stat failed for file \"" + file + "\"";
-        LOG_ERROR(error);
-        throw WException(error);
-      }
-      return (unsigned long long)sb.st_size;
-#endif //WT_HAVE_POSIX_FILEIO
     }
 
     std::string* fileToString(const std::string& fileName)
@@ -77,53 +60,24 @@ namespace Wt {
 
     time_t lastWriteTime(const std::string &file)
     {
-#ifndef WT_HAVE_POSIX_FILEIO
-      return (unsigned long long)boost::filesystem::last_write_time(file);
-#else //WT_HAVE_POSIX_FILEIO
-      struct stat sb;
-      if (stat(file.c_str(), &sb) == -1) {
-        std::string error
-          = "lastWriteTime: stat failed for file \"" + file + "\"";
-        LOG_ERROR(error);
-        throw WException(error);
-      }
-      return (unsigned long long)sb.st_mtime;
-#endif //WT_HAVE_POSIX_FILEIO
+      return boost::filesystem::last_write_time(file);
     }
 
     bool exists(const std::string &file)
     {
-#ifndef WT_HAVE_POSIX_FILEIO
       boost::filesystem::path path(file);
       return boost::filesystem::exists(path);
-#else //WT_HAVE_POSIX_FILEIO
-      struct stat sb;
-      return stat(file.c_str(), &sb) != -1;
-#endif //WT_HAVE_POSIX_FILEIO
     }
 
     bool isDirectory(const std::string &file)
     {
-#ifndef WT_HAVE_POSIX_FILEIO
       boost::filesystem::path path(file);
       return boost::filesystem::is_directory(path);
-#else //WT_HAVE_POSIX_FILEIO
-      struct stat sb;
-      stat(file.c_str(), &sb);
-      if (stat(file.c_str(), &sb) == -1) {
-        std::string error
-          = "isDirectory: stat failed for file \"" + file + "\"";
-        LOG_ERROR(error);
-        throw WException(error);
-      }
-      return S_ISDIR(sb.st_mode);
-#endif //WT_HAVE_POSIX_FILEIO
     }
 
     void listFiles(const std::string &directory,
                    std::vector<std::string> &files)
     {
-#ifndef WT_HAVE_POSIX_FILEIO
       boost::filesystem::path path(directory);
       boost::filesystem::directory_iterator end_itr;
 
@@ -138,21 +92,6 @@ namespace Wt {
         std::string f = (*i).path().string();
         files.push_back(f);
       }
-#else //WT_HAVE_POSIX_FILEIO
-      DIR *dp;
-      struct dirent *dirp;
-      if((dp = opendir(directory.c_str())) == NULL) {
-        std::string error
-           = "listFiles: opendir failed for file \"" + directory + "\"";
-        LOG_ERROR(error);
-        throw WException(error);
-      }
-
-      while ((dirp = readdir(dp)) != NULL)
-        files.push_back(dirp->d_name);
-
-      closedir(dp);
-#endif //WT_HAVE_POSIX_FILEIO
     }
 
     std::string getTempDir()
