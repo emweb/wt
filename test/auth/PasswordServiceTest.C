c1e6550451672f8c4e2a1f24caec6473261298f2
matthias <matthias@emweb.be>
2024-10-21 10:34:11 +0200

    WT-13130: Remove a timing-dependent unstable test
    Depending on the platform and its scheduler this test sometimes failed,
    resulting in unstable testing behaviour. Test should be stable, and
    reproducible.

--- test/auth/PasswordServiceTest.C
+++ test/auth/PasswordServiceTest.C
@@ -117,27 +117,3 @@ BOOST_AUTO_TEST_CASE( throttle_enabled_failure_test )
   }
 }
 
-BOOST_AUTO_TEST_CASE( throttle_enabled_failure_with_last_attempt_offset_test )
-{
-  PasswordDboFixture f;
-  f.myPasswordService_->setPasswordThrottle(std::make_unique<Auth::AuthThrottle>());
-
-  Wt::Dbo::Transaction transaction(*f.session_);
-  Auth::User user = f.users_->registerNew();
-  transaction.commit();
-
-  std::vector<int> attemptResults { 0, 3, 7, 21, 20 };
-
-  // Test for 5 failure attempts
-  for (std::size_t failures = 0; failures < 5; ++failures) {
-    // Have one additional login failure
-    user.setAuthenticated(false);
-    transaction.commit();
-
-    // Sleep for x number of seconds, to test current datetime offset.
-    std::this_thread::sleep_for(std::chrono::seconds(failures + 1));
-
-    BOOST_REQUIRE(f.myPasswordService_->delayForNextAttempt(user) == attemptResults[failures]);
-  }
-}
-
