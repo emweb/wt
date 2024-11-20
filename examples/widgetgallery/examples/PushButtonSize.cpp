7343f64d1eb055df6f035d88c344eb613dfd3394
Romain Mardulyn <romain@legion.leuven.emweb.be>
2024-11-20 16:55:21 +0100

    WT-12953: Fix button in button size example
    In the button size example of the widgetgallery, fix the button size not
    changing when using bootstrap2 and fix color of small and large button
    being white when using bootstrap 5.

--- examples/widgetgallery/examples/PushButtonSize.cpp
+++ examples/widgetgallery/examples/PushButtonSize.cpp
@@ -6,14 +6,14 @@ auto result =
     std::make_unique<Wt::WTemplate>(Wt::WString::tr("pushButtonSize-template"));
 
 auto button = std::make_unique<Wt::WPushButton>("Large");
-button->setStyleClass("btn-lg");
+button->setStyleClass("btn-lg btn-large btn-secondary");
 result->bindWidget("button-large", std::move(button));
 
 button = std::make_unique<Wt::WPushButton>("Default");
 result->bindWidget("button-default", std::move(button));
 
 button = std::make_unique<Wt::WPushButton>("Small");
-button->setStyleClass("btn-sm");
+button->setStyleClass("btn-sm btn-small btn-secondary");
 result->bindWidget("button-small", std::move(button));
 
 SAMPLE_END(return std::move(result))
