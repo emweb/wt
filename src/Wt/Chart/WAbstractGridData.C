963bd14a7938f759046ab7ec2bf180cf0b231c8c
Romain Mardulyn <romain@legion.leuven.emweb.be>
2025-01-02 15:58:05 +0100

    WT-10954: keep fallthrough in WAbstractGridData
    This fallthrough is necessary. It can be seen in the chart3D examples
    when using the "Isotope data (10x10) with ColorRoles" dataset with
    categoryExample, the bar supposed to be blue are transparent instead.

--- src/Wt/Chart/WAbstractGridData.C
+++ src/Wt/Chart/WAbstractGridData.C
@@ -288,7 +288,6 @@ void WAbstractGridData::updateGL()
     chart_->useProgram(meshProgram_);
     chart_->uniformMatrix4(mesh_mvMatrixUniform_, mvMatrix_);
     chart_->uniformMatrix4(mesh_pMatrix_, chart_->pMatrix());
-    // FIXME: suspicious fallthrough
     WT_FALLTHROUGH
   case Series3DType::Point:
     chart_->useProgram(colSeriesProgram_);
