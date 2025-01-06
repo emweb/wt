1d139232d5d9f677b4f50f658c20d94705c81197
Romain Mardulyn <romain@legion.leuven.emweb.be>
2025-01-06 13:02:54 +0100

    WT-12945: update widget gallery example
    Update the WTree widget gallery example to show how to use WIconPair
    with Font-Awesome Icons

--- examples/widgetgallery/examples/Tree.cpp
+++ examples/widgetgallery/examples/Tree.cpp
@@ -1,10 +1,13 @@
+#include <Wt/WContainerWidget.h>
 #include <Wt/WIconPair.h>
+#include <Wt/WPushButton.h>
 #include <Wt/WText.h>
 #include <Wt/WTree.h>
 #include <Wt/WTreeNode.h>
 
 SAMPLE_BEGIN(Tree)
-std::unique_ptr<Wt::WTree> tree = std::make_unique<Wt::WTree>();
+std::unique_ptr<Wt::WContainerWidget> container = std::make_unique<Wt::WContainerWidget>();
+Wt::WTree *tree = container->addWidget(std::make_unique<Wt::WTree>());
 
 tree->setSelectionMode(Wt::SelectionMode::Extended);
 
@@ -14,6 +17,7 @@ auto folderIcon
 
 auto node =
     std::make_unique<Wt::WTreeNode>("Furniture",std::move(folderIcon));
+auto furnitureNode = node.get();
 tree->setTreeRoot(std::move(node));
 
 
@@ -35,4 +39,21 @@ subtree_->addChildNode(std::make_unique<Wt::WTreeNode>("Dopey"));
 subtree_->addChildNode(std::make_unique<Wt::WTreeNode>("Bashful"));
 subtree_->addChildNode(std::make_unique<Wt::WTreeNode>("Sleepy"));
 
-SAMPLE_END(return std::move(tree))
+auto imageButton = container->addWidget(std::make_unique<Wt::WPushButton>("Use Image Icons"));
+imageButton->clicked().connect([=](){
+    auto icon
+    = std::make_unique<Wt::WIconPair>("icons/yellow-folder-closed.png",
+                        "icons/yellow-folder-open.png", false);
+    furnitureNode->setLabelIcon(std::move(icon));
+});
+
+auto FAButton = container->addWidget(std::make_unique<Wt::WPushButton>("Use Font-Awesome Icons"));
+FAButton->clicked().connect([=](){
+    auto icon
+    = std::make_unique<Wt::WIconPair>("folder",
+                        "folder-open", false);
+    icon->setIconsType(Wt::WIconPair::IconType::IconName);
+    furnitureNode->setLabelIcon(std::move(icon));
+});
+
+SAMPLE_END(return std::move(container))
