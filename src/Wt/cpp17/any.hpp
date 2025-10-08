603d8747eef3b6be5864b43c2d2eaa3ac6abfef8
Romain Mardulyn <romain@legion.leuven.emweb.be>
2025-10-08 15:58:50 +0200

    WT-13666: Deprecate cpp17::any any implementation
    Since Wt must now be compiled with C++17 or above, only std::any or
    should be used. Therefore, cpp17::any is marked as deprecated and will
    be removed in a future version.
    
    We however cannot deprecate the version that of cpp17 that uses std::any
    so that there is a way to avoid all of the warnings. We will mark that
    as deprecated once the other options are removed.

--- src/Wt/cpp17/any.hpp
+++ src/Wt/cpp17/any.hpp
@@ -8,6 +8,7 @@
 #define WT_CPP17_ANY_HPP
 
 #include "Wt/WConfig.h"
+#include "Wt/WDllDefs.h"
 
 #if defined(WT_ANY_IS_EXPERIMENTAL_ANY)
 
@@ -15,30 +16,40 @@
 
 namespace Wt {
   namespace cpp17 {
-    using any = std::experimental::any;
-    using bad_any_cast = std::experimental::bad_any_cast;
+    using any
+      WT_DEPRECATED("Wt::cpp17::any is deprecated and will be removed in a future version. Use std::any instead")
+      = std::experimental::any;
+    using bad_any_cast
+      WT_DEPRECATED("Wt::cpp17::bad_any_cast is deprecated and will be removed in a future version. Use std::bad_any_cast instead")
+      = std::experimental::bad_any_cast;
 
+    WT_DEPRECATED("Wt::cpp17::any_has_value is deprecated and will be removed in a future version. Use std::any_has_value instead")
     inline bool any_has_value(const any &v) {
       return !v.empty();
     }
 
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     ValueType any_cast(const any& operand) {
       return std::experimental::any_cast<ValueType>(operand);
     }
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     ValueType any_cast(any& operand) {
       return std::experimental::any_cast<ValueType>(operand);
     }
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     ValueType any_cast(any&& operand) {
       return std::experimental::any_cast<ValueType>(std::move(operand));
     }
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     const ValueType* any_cast(const any* operand) noexcept {
       return std::experimental::any_cast<ValueType>(operand);
     }
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     ValueType* any_cast(any* operand) noexcept {
       return std::experimental::any_cast<ValueType>(operand);
     }
@@ -87,30 +98,40 @@ namespace Wt {
 
 namespace Wt {
   namespace cpp17 {
-    using any = linb::any;
-    using bad_any_cast = linb::bad_any_cast;
+    using any
+      WT_DEPRECATED("Wt::cpp17::any is deprecated and will be removed in a future version. Use std::any instead")
+      = linb::any;
+    using bad_any_cast
+      WT_DEPRECATED("Wt::cpp17::bad_any_cast is deprecated and will be removed in a future version. Use std::bad_any_cast instead")
+      = linb::bad_any_cast;
 
+    WT_DEPRECATED("Wt::cpp17::any_has_value is deprecated and will be removed in a future version. Use std::any_has_value instead")
     inline bool any_has_value(const any &v) {
       return !v.empty();
     }
 
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     ValueType any_cast(const any& operand) {
       return linb::any_cast<ValueType>(operand);
     }
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     ValueType any_cast(any& operand) {
       return linb::any_cast<ValueType>(operand);
     }
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     ValueType any_cast(any&& operand) {
       return linb::any_cast<ValueType>(std::move(operand));
     }
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     const ValueType* any_cast(const any* operand) noexcept {
       return linb::any_cast<ValueType>(operand);
     }
     template<class ValueType>
+    WT_DEPRECATED("Wt::cpp17::any_cast is deprecated and will be removed in a future version. Use std::any_cast instead")
     ValueType* any_cast(any* operand) noexcept {
       return linb::any_cast<ValueType>(operand);
     }
