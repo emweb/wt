e9cdaafad5954af3f155fd5edc0e7bcd06bb1bdb
Romain Mardulyn <romain@legion.leuven.emweb.be>
2025-10-29 16:17:59 +0100

    WT-14074: Test disabling locale when parsing dates

--- test/types/WDateTest.C
+++ test/types/WDateTest.C
@@ -11,16 +11,25 @@
 
 #include <web/FileUtils.h>
 
+namespace {
+
+  void setup(Wt::WApplication& app)
+  {
+    std::string file = app.appRoot() + "types/days_months_names";
+    BOOST_REQUIRE(Wt::FileUtils::exists(file + ".xml"));
+
+    app.messageResourceBundle().use(file);
+  }
+
+}
+
 BOOST_AUTO_TEST_CASE( WDate_toString_non_localized )
 {
   // Regression test for issue #11852
   // This produces a string that disregards the locale
   Wt::Test::WTestEnvironment environment;
   Wt::WApplication app(environment);
-  std::string file = app.appRoot() + "types/days_months_names";
-  BOOST_REQUIRE(Wt::FileUtils::exists(file + ".xml"));
-
-  app.messageResourceBundle().use(file);
+  setup(app);
 
   Wt::WDate date(2022, 1, 2);
   BOOST_TEST(date.toString("dd/MM/yyyy", false) == "02/01/2022");
@@ -35,10 +44,7 @@ BOOST_AUTO_TEST_CASE( WDate_toString_localized )
   // This produces a string that takes the locale into account
   Wt::Test::WTestEnvironment environment;
   Wt::WApplication app(environment);
-  std::string file = app.appRoot() + "types/days_months_names";
-  BOOST_REQUIRE(Wt::FileUtils::exists(file + ".xml"));
-
-  app.messageResourceBundle().use(file);
+  setup(app);
 
   Wt::WDate date(2022, 1, 2);
   BOOST_TEST(date.toString() == "Dom Ene 2 2022");
@@ -46,4 +52,50 @@ BOOST_AUTO_TEST_CASE( WDate_toString_localized )
   // January 2nd 2022 is a Sunday
   BOOST_TEST(date.toString("ddd MMM") == "Dom Ene");
   BOOST_TEST(date.toString("dddd MMMM") == "Domigo Enero");
+}
+
+BOOST_AUTO_TEST_CASE( WDate_fromString_non_localized )
+{
+  // This parse a string disregarding the locale
+  Wt::Test::WTestEnvironment environment;
+  Wt::WApplication app(environment);
+  setup(app);
+
+  Wt::WDate date(2022, 1, 2);
+  // Ensure date is valid
+  BOOST_REQUIRE(date != Wt::WDate());
+
+  BOOST_TEST((Wt::WDate::fromString("02/01/2022", "dd/MM/yyyy", false) == date));
+
+  // Check that English names are recognized
+  // January 2nd 2022 is a Sunday
+  BOOST_TEST((Wt::WDate::fromString("Sun 02 Jan 2022", "ddd dd MMM yyyy", false) == date));
+  BOOST_TEST((Wt::WDate::fromString("Sunday 02 January 2022", "dddd dd MMMM yyyy", false) == date));
+
+  // Check that localized names are not recognized
+  BOOST_TEST((Wt::WDate::fromString("Dom 02 Ene 2022", "ddd dd MMM yyyy", false) == Wt::WDate()));
+  BOOST_TEST((Wt::WDate::fromString("Domigo 02 Enero 2022", "dddd dd MMMM yyyy", false) == Wt::WDate()));
+}
+
+BOOST_AUTO_TEST_CASE( WDate_fromString_localized )
+{
+  // This parse a string taking the locale into account
+  Wt::Test::WTestEnvironment environment;
+  Wt::WApplication app(environment);
+  setup(app);
+
+  Wt::WDate date(2022, 1, 2);
+  // Ensure date is valid
+  BOOST_REQUIRE(date != Wt::WDate());
+
+  BOOST_TEST((Wt::WDate::fromString("02/01/2022", "dd/MM/yyyy") == date));
+
+  // Check that localized names are recognized
+  BOOST_TEST((Wt::WDate::fromString("Dom 02 Ene 2022", "ddd dd MMM yyyy") == date));
+  BOOST_TEST((Wt::WDate::fromString("Domigo 02 Enero 2022", "dddd dd MMMM yyyy") == date));
+
+  // Check that English names not recognized
+  // January 2nd 2022 is a Sunday
+  BOOST_TEST((Wt::WDate::fromString("Sun 02 Jan 2022", "ddd dd MMM yyyy") == Wt::WDate()));
+  BOOST_TEST((Wt::WDate::fromString("Sunday 02 January 2022", "dddd dd MMMM yyyy") == Wt::WDate()));
 }
--- test/types/WDateTimeTest.C
+++ test/types/WDateTimeTest.C
@@ -13,16 +13,25 @@
 
 #include <web/FileUtils.h>
 
+namespace {
+
+  void setup(Wt::WApplication& app)
+  {
+    std::string file = app.appRoot() + "types/days_months_names";
+    BOOST_REQUIRE(Wt::FileUtils::exists(file + ".xml"));
+
+    app.messageResourceBundle().use(file);
+  }
+
+}
+
 BOOST_AUTO_TEST_CASE( WDateTime_toString_non_localized )
 {
   // Regression test for issue #11852
   // This produces a string that disregards the locale
   Wt::Test::WTestEnvironment environment;
   Wt::WApplication app(environment);
-  std::string file = app.appRoot() + "types/days_months_names";
-  BOOST_REQUIRE(Wt::FileUtils::exists(file + ".xml"));
-
-  app.messageResourceBundle().use(file);
+  setup(app);
 
   Wt::WDateTime datetime(Wt::WDate(2022, 1, 2), Wt::WTime(10, 8, 9));
   BOOST_TEST(datetime.toString("hh:mm:ss dd/MM/yyyy", false) == "10:08:09 02/01/2022");
@@ -37,10 +46,7 @@ BOOST_AUTO_TEST_CASE( WDateTime_toString_localized )
   // This produces a string that disregards the locale
   Wt::Test::WTestEnvironment environment;
   Wt::WApplication app(environment);
-  std::string file = app.appRoot() + "types/days_months_names";
-  BOOST_REQUIRE(Wt::FileUtils::exists(file + ".xml"));
-
-  app.messageResourceBundle().use(file);
+  setup(app);
 
   Wt::WDateTime datetime(Wt::WDate(2022, 1, 2), Wt::WTime(10, 8, 9));
   BOOST_TEST(datetime.toString() == "Dom Ene 2 10:08:09 2022");
@@ -48,4 +54,50 @@ BOOST_AUTO_TEST_CASE( WDateTime_toString_localized )
   // January 2nd 2022 is a Sunday
   BOOST_TEST(datetime.toString("ddd MMM") == "Dom Ene");
   BOOST_TEST(datetime.toString("dddd MMMM") == "Domigo Enero");
+}
+
+BOOST_AUTO_TEST_CASE( WDateTime_fromString_non_localized )
+{
+  // This parse a string disregarding the locale
+  Wt::Test::WTestEnvironment environment;
+  Wt::WApplication app(environment);
+  setup(app);
+
+  Wt::WDateTime datetime(Wt::WDate(2022, 1, 2), Wt::WTime(10, 8, 9));
+  // Ensure datetime is valid
+  BOOST_REQUIRE(datetime != Wt::WDateTime());
+
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 02/01/2022", "hh:mm:ss dd/MM/yyyy", false) == datetime));
+
+  // Check that English names are recognized
+  // January 2nd 2022 is a Sunday
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 Sun 02 Jan 2022", "hh:mm:ss ddd dd MMM yyyy", false) == datetime));
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 Sunday 02 January 2022", "hh:mm:ss dddd dd MMMM yyyy", false) == datetime));
+
+  // Check that localized names are not recognized
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 Dom 02 Ene 2022", "hh:mm:ss ddd dd MMM yyyy", false) == Wt::WDateTime()));
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 Domigo 02 Enero 2022", "hh:mm:ss dddd dd MMMM yyyy", false) == Wt::WDateTime()));
+}
+
+BOOST_AUTO_TEST_CASE( WDateTime_fromString_localized )
+{
+  // This parse a string taking the locale into account
+  Wt::Test::WTestEnvironment environment;
+  Wt::WApplication app(environment);
+  setup(app);
+
+  Wt::WDateTime datetime(Wt::WDate(2022, 1, 2), Wt::WTime(10, 8, 9));
+  // Ensure datetime is valid
+  BOOST_REQUIRE(datetime != Wt::WDateTime());
+
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 02/01/2022", "hh:mm:ss dd/MM/yyyy") == datetime));
+
+  // Check that localized names are recognized
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 Dom 02 Ene 2022", "hh:mm:ss ddd dd MMM yyyy") == datetime));
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 Domigo 02 Enero 2022", "hh:mm:ss dddd dd MMMM yyyy") == datetime));
+
+  // Check that English names not recognized
+  // January 2nd 2022 is a Sunday
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 Sun 02 Jan 2022", "hh:mm:ss ddd dd MMM yyyy") == Wt::WDateTime()));
+  BOOST_TEST((Wt::WDateTime::fromString("10:08:09 Sunday 02 January 2022", "hh:mm:ss dddd dd MMMM yyyy") == Wt::WDateTime()));
 }
