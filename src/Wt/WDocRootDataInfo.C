9c88ab0b8a4040d6b53d2e184d900159f26964c6
erikbs <erikbsolem@hotmail.com>
2026-01-30 04:10:12 +0100

    WDocRootDataInfo.C: use cpp17::filesystem instead of boost::filesystem

--- src/Wt/WDocRootDataInfo.C
+++ src/Wt/WDocRootDataInfo.C
@@ -6,10 +6,10 @@
 
 #include "Wt/WDocRootDataInfo.h"
 
+#include "Wt/cpp17/filesystem.hpp"
+
 #include "Wt/WApplication.h"
 #include "Wt/WException.h"
-
-#include <boost/filesystem/path.hpp>
 
 namespace Wt {
 
@@ -25,8 +25,8 @@ void WDocRootDataInfo::setRelativePath(const std::string& path)
 std::string WDocRootDataInfo::filePath() const
 {
   if (hasFilePath()) {
-    boost::filesystem::path path = boost::filesystem::path(relPath_);
-    path = boost::filesystem::path(WApplication::instance()->docRoot()) / path;
+    cpp17::filesystem::path path = cpp17::filesystem::path(relPath_);
+    path = cpp17::filesystem::path(WApplication::instance()->docRoot()) / path;
     return path.string();
   }
   return WAbstractDataInfo::filePath();
