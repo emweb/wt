b706a887c550d957bf33a111d607999bfbb473ef
Romain Mardulyn <romain@legion.leuven.emweb.be>
2026-03-06 10:52:52 +0100

    WT-14352: Fix lastWriteTime clock convertion
    The convertion from a file_clock to a system_clock was done in several
    step for the time point, one of which lead to rounding. Using
    std::chrono::clock_cast() for the conversion is better as it does not
    cause any rounding.

--- src/web/FileUtils.C
+++ src/web/FileUtils.C
@@ -70,9 +70,7 @@ namespace Wt {
     {
 #ifdef WT_FILESYSTEM_IMPL_STD
   #ifndef WT_FILESYSTEM_IMPL_STD_CLOCK_17
-      auto ftime = Wt::cpp17::filesystem::last_write_time(file);
-      auto systime = decltype(ftime)::clock::to_sys(ftime);
-      return std::chrono::system_clock::from_time_t(decltype(systime)::clock::to_time_t(systime));
+      return std::chrono::clock_cast<std::chrono::system_clock>(Wt::cpp17::filesystem::last_write_time(file));
   #else // WT_FILESYSTEM_IMPL_STD_CLOCK_17
       LOG_DEBUG("When using cpp17 or lower with std::filesystem, the result of this function is an approximation. Use boost::filesystem, instead of std::filesystem (see WT_CPP17_FILESYSTEM_IMPLEMENTATION) if this is a problem for your application.");
       auto ftime = Wt::cpp17::filesystem::last_write_time(file);
