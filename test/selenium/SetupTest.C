bae53fda97493a29dc24e7431651bc17152c573e
matthias <matthias@emweb.be>
2026-03-09 21:22:23 +0100

    WT-14383: Add a setup test
    This test verifies the server (via the fixture, using the wrapper) is
    correctly set up, and destructed.

---
+++ test/selenium/SetupTest.C
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2026 Emweb bv, Herent, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+#include "framework/SeleniumTest.h"
+
+// This test ensures that the SeleniumFixture correctly sets up the
+// server.
+SELENIUM_TEST(selenium_setup, Wt::WApplication)
+END_SELENIUM_TEST
