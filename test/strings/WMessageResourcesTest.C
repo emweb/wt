06901d7c54e77a6cfcb83d7b043a0f496b039fb4
Romain Mardulyn <romain@legion.leuven.emweb.be>
2026-03-30 14:49:52 +0200

    WT-14427: Add test for WMessageResource division
    Add the fuzzer test as well as a division by 0 test.

---
+++ test/strings/WMessageResourcesTest.C
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2026 Emweb bv, Herent, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+#include <boost/test/unit_test.hpp>
+
+#include <Wt/WMessageResources.h>
+#include <Wt/Exception/WInvalidOperationException.h>
+
+
+BOOST_AUTO_TEST_CASE(test_WMessageResources_evalPluralCase_overflow_division)
+{
+  std::string msg = "9088*272*3088*768*36*576*1088*36352*8*752*32*32/(1-2)";
+  BOOST_CHECK_EQUAL(Wt::WMessageResources::evalPluralCase(msg, msg.size()), 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_WMessageResources_evalPluralCase_overflow_modulo)
+{
+  std::string msg = "9088*272*3088*768*36*576*1088*36352*8*752*32*32%(1-2)";
+  BOOST_CHECK_EQUAL(Wt::WMessageResources::evalPluralCase(msg, msg.size()), 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_WMessageResources_evalPluralCase_divide_by_zero)
+{
+  std::string msg = "9088*272*3088*768*36*576*1088*36352*8*752*32*32/(1-1)";
+  BOOST_CHECK_THROW(Wt::WMessageResources::evalPluralCase(msg, msg.size()), Wt::WInvalidOperationException);
+}
+
+BOOST_AUTO_TEST_CASE(test_WMessageResources_evalPluralCase_modulo_by_zero)
+{
+  std::string msg = "9088*272*3088*768*36*576*1088*36352*8*752*32*32%(1-1)";
+  BOOST_CHECK_THROW(Wt::WMessageResources::evalPluralCase(msg, msg.size()), Wt::WInvalidOperationException);
+}
