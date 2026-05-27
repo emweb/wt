e3446d659fa12d3182df5cb6e5896ff81b435835
Romain Mardulyn <romain@legion.leuven.emweb.be>
2026-05-27 14:25:17 +0200

    WT-14213: Add test for Dbo::Query::with()

--- test/dbo/DboTest9.C
+++ test/dbo/DboTest9.C
@@ -11,34 +11,62 @@
 
 namespace dbo = Wt::Dbo;
 
-class Painting
-{
-public:
-  std::string name;
-  std::string artist;
-  int price;
 
-  template<typename Action>
-  void persist(Action &a)
+namespace {
+  class Painting
   {
-    dbo::field(a, name, "name");
-    dbo::field(a, artist, "artist");
-    dbo::field(a, price, "price");
-  }
-};
+  public:
+    std::string name;
+    std::string artist;
+    int price;
 
-struct Dbo9Fixture : DboFixtureBase {
-  Dbo9Fixture()
-  {
-    session_->mapClass<Painting>("painting");
-
-    try {
-      session_->dropTables();
-    } catch (...) {
+    template<typename Action>
+    void persist(Action &a)
+    {
+      dbo::field(a, name, "name");
+      dbo::field(a, artist, "artist");
+      dbo::field(a, price, "price");
     }
-    session_->createTables();
-  }
-};
+  };
+
+  struct Dbo9Fixture : DboFixtureBase {
+    Dbo9Fixture()
+    {
+      session_->mapClass<Painting>("painting");
+
+      try {
+        session_->dropTables();
+      } catch (...) {
+      }
+      session_->createTables();
+
+      {
+        dbo::Transaction t(*session_);
+
+        dbo::ptr<Painting> p1 = session_->addNew<Painting>();
+        p1.modify()->name = "Zonnebloemen";
+        p1.modify()->artist = "Van Gogh";
+        p1.modify()->price = 1000;
+
+        dbo::ptr<Painting> p2 = session_->addNew<Painting>();
+        p2.modify()->name = "De sterrennacht";
+        p2.modify()->artist = "Van Gogh";
+        p2.modify()->price = 5000;
+
+        dbo::ptr<Painting> p3 = session_->addNew<Painting>();
+        p3.modify()->name = "Het melkmeisje";
+        p3.modify()->artist = "Vermeer";
+        p3.modify()->price = 2000;
+
+        dbo::ptr<Painting> p4 = session_->addNew<Painting>();
+        p4.modify()->name = "Meisje met de parel";
+        p4.modify()->artist = "Vermeer";
+        p4.modify()->price = 3000;
+      }
+    }
+  };
+
+}
 
 BOOST_AUTO_TEST_SUITE( DBO_TEST_SUITE_NAME )
 
@@ -46,30 +74,6 @@ BOOST_AUTO_TEST_CASE( dbo9_test1_subquery )
 {
   Dbo9Fixture f;
   dbo::Session &session = *f.session_;
-
-  {
-    dbo::Transaction t(session);
-
-    dbo::ptr<Painting> p1 = session.addNew<Painting>();
-    p1.modify()->name = "Zonnebloemen";
-    p1.modify()->artist = "Van Gogh";
-    p1.modify()->price = 1000;
-
-    dbo::ptr<Painting> p2 = session.addNew<Painting>();
-    p2.modify()->name = "De sterrennacht";
-    p2.modify()->artist = "Van Gogh";
-    p2.modify()->price = 5000;
-
-    dbo::ptr<Painting> p3 = session.addNew<Painting>();
-    p3.modify()->name = "Het melkmeisje";
-    p3.modify()->artist = "Vermeer";
-    p3.modify()->price = 2000;
-
-    dbo::ptr<Painting> p4 = session.addNew<Painting>();
-    p4.modify()->name = "Meisje met de parel";
-    p4.modify()->artist = "Vermeer";
-    p4.modify()->price = 3000;
-  }
 
   {
     dbo::Transaction t(session);
@@ -96,6 +100,53 @@ BOOST_AUTO_TEST_CASE( dbo9_test1_subquery )
       .where("p.price > (" + maxPriceQuery.asString() + ")").bindSubqueryValues(maxPriceQuery);
     auto results = q.resultList();
     BOOST_TEST(results.size() == 1);
+    auto result = results.front();
+    BOOST_TEST(result->name == "De sterrennacht");
+    BOOST_TEST(result->artist == "Van Gogh");
+    BOOST_TEST(result->price == 5000);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( dbo9_query_using_single_with )
+{
+  Dbo9Fixture f;
+  dbo::Session &session = *f.session_;
+
+  {
+    dbo::Transaction t(session);
+
+    auto q = session.query<dbo::ptr<Painting>>(
+      "select p from \"painting\" as p "
+      "where p.price > (select MAX(ap.price) from artist_paintings as ap)")
+      .with("artist_paintings", "select * from \"painting\" where artist = ?")
+      .bind("Vermeer");
+
+    auto results = q.resultList();
+    BOOST_REQUIRE(results.size() == 1);
+    auto result = results.front();
+    BOOST_TEST(result->name == "De sterrennacht");
+    BOOST_TEST(result->artist == "Van Gogh");
+    BOOST_TEST(result->price == 5000);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( dbo9_query_using_single_multiple_with )
+{
+  Dbo9Fixture f;
+  dbo::Session &session = *f.session_;
+
+  {
+    dbo::Transaction t(session);
+
+    auto q = session.query<dbo::ptr<Painting>>(
+      "select p from \"painting\" as p "
+      "where p.price > (select mp.price from max_price as mp)")
+      .with("artist_paintings", "select * from \"painting\" where artist = ?")
+      .with("max_price (price)", "select MAX(ap.price) from artist_paintings as ap")
+      .bind("Vermeer");
+
+    auto results = q.resultList();
+    BOOST_REQUIRE(results.size() == 1);
     auto result = results.front();
     BOOST_TEST(result->name == "De sterrennacht");
     BOOST_TEST(result->artist == "Van Gogh");
