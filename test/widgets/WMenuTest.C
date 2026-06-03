0891d5364125bbf469989677301d28e8f95b195f
Romain Mardulyn <romain@legion.leuven.emweb.be>
2026-06-03 12:57:25 +0200

    WT-14576: Add test for WMenu
    Add a test that checks if WMenu::addItem does correctly change the
    current index when needed.

---
+++ test/widgets/WMenuTest.C
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2026 Emweb bv, Herent, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+#include <boost/test/unit_test.hpp>
+
+#include <Wt/Test/WTestEnvironment.h>
+#include <Wt/WApplication.h>
+#include <Wt/WMenu.h>
+#include <Wt/WStackedWidget.h>
+#include <Wt/WText.h>
+
+BOOST_AUTO_TEST_CASE(WMenu_addItem_change_index_for_internal_path_match)
+{
+  Wt::Test::WTestEnvironment testEnv;
+  testEnv.setInternalPath("/links");
+  Wt::WApplication app(testEnv);
+
+  Wt::WStackedWidget *stack = app.root()->addNew<Wt::WStackedWidget>();
+  Wt::WMenu *menu = app.root()->addNew<Wt::WMenu>(stack);
+  menu->setInternalPathEnabled("/");
+
+  BOOST_TEST(menu->currentIndex() == -1);
+
+  menu->addItem("dashboard", std::make_unique<Wt::WText>("Dashboard"));
+
+  BOOST_TEST(menu->currentIndex() == 0);
+
+  // Check that adding an item with the internal path changes the index
+  menu->addItem("links", std::make_unique<Wt::WText>("Links"));
+
+  BOOST_TEST(menu->currentIndex() == 1);
+
+  // Check that adding an item with an empty path component does not change the index
+  int previousIndex = menu->currentIndex();
+  menu->addItem("", std::make_unique<Wt::WText>("Account Settings Page"));
+
+  BOOST_TEST(menu->currentIndex() == previousIndex);
+}
+
